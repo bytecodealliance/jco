bytecodealliance/wasmtime-py

Refactor and reorganize how testing works (#363)
This commit is a large refactor of how testing works in this repository
for wit-bindgen and hosts/guests to simplify the flow of tests and make
it easier to modify and less monolithic. Previously tons of logic was
located in a `test_helpers` macro crate but now that large crate has
been split up to delegate more work to each individual test. Some common
functionality is still present in a `test_helpers` crate but it's more
clear that the `test_helpers::codegen_test` macro is purely responsible
for simply expanding a glob at compile time and delegating to a
macro-per-test.

The tests of Rust & other languages are now also more uniform where
everything "just invokes a macro" where Rust's macros further invoke
proc-macros for the respective generators and other languages delegate
to shared functionality for "generate code" then "run verification
program".

Overall I'm hoping this makes the testing easier to follow and
understand, and in upcoming changes for components it will make it
easier to change each generator, individually, over to components
ideally.

bindgen/crates/gen-host-wasmtime-py/tests/codegen.rs
@@ -1,52 +1,45 @@
 use std::path::Path;
 use std::process::Command;
 
-#[rustfmt::skip]
+macro_rules! gen_test {
+    ($name:ident $test:tt $dir:ident) => {
+        #[test]
+        fn $name() {
+            test_helpers::run_codegen_test(
+                "wasmtime-py",
+                std::path::Path::new($test)
+                    .file_stem()
+                    .unwrap()
+                    .to_str()
+                    .unwrap(),
+                include_str!($test),
+                test_helpers::Direction::$dir,
+                wit_bindgen_gen_host_wasmtime_py::Opts::default().build(),
+                super::verify,
+            )
+        }
+    };
+}
+
 mod exports {
-    test_helpers::codegen_py_export!(
-        "*.wit"
-
-        // If you want to exclude a specific test you can include it here with
-        // gitignore glob syntax:
-        //
-        // "!wasm.wit"
-        // "!host.wit"
-        //
-        //
-        // Similarly you can also just remove the `*.wit` glob and list tests
-        // individually if you're debugging.
-    );
-}
-
-#[rustfmt::skip]
-mod imports {
-    test_helpers::codegen_py_import!(
-        "*.wit"
-    );
-}
-
-fn verify(dir: &str, _name: &str) {
-    let output = Command::new("mypy")
-        .arg(Path::new(dir).join("bindings.py"))
-        .arg("--config-file")
-        .arg("mypy.ini")
-        .output()
-        .expect("failed to run `mypy`; do you have it installed?");
-    if output.status.success() {
-        return;
+    macro_rules! codegen_test {
+        ($name:ident $test:tt) => (gen_test!($name $test Export);)
     }
-    panic!(
-        "mypy failed
+    test_helpers::codegen_tests!("*.wit");
+}
 
-status: {status}
+mod imports {
+    macro_rules! codegen_test {
+        ($name:ident $test:tt) => (gen_test!($name $test Import);)
+    }
+    test_helpers::codegen_tests!("*.wit");
+}
 
-stdout ---
-{stdout}
-
-stderr ---
-{stderr}",
-        status = output.status,
-        stdout = String::from_utf8_lossy(&output.stdout).replace("\n", "\n\t"),
-        stderr = String::from_utf8_lossy(&output.stderr).replace("\n", "\n\t"),
+fn verify(dir: &Path, _name: &str) {
+    test_helpers::run_command(
+        Command::new("mypy")
+            .arg(dir.join("bindings.py"))
+            .arg("--config-file")
+            .arg("mypy.ini"),
     );
 }

bytecodealliance/jco

crates/gen-host-js/tests/codegen.rs
@@ -1,44 +1,53 @@
 use std::path::Path;
 use std::process::Command;
 
-#[rustfmt::skip]
+macro_rules! gen_test {
+    ($name:ident $test:tt $dir:ident) => {
+        #[test]
+        fn $name() {
+            test_helpers::run_codegen_test(
+                "js",
+                std::path::Path::new($test)
+                    .file_stem()
+                    .unwrap()
+                    .to_str()
+                    .unwrap(),
+                include_str!($test),
+                test_helpers::Direction::$dir,
+                wit_bindgen_gen_host_js::Opts::default().build(),
+                super::verify,
+            )
+        }
+    };
+}
+
 mod exports {
-    test_helpers::codegen_js_export!(
-        "*.wit"
-
-        // If you want to exclude a specific test you can include it here with
-        // gitignore glob syntax:
-        //
-        // "!wasm.wit"
-        // "!host.wit"
-        //
-        //
-        // Similarly you can also just remove the `*.wit` glob and list tests
-        // individually if you're debugging.
-    );
+    macro_rules! codegen_test {
+        ($name:ident $test:tt) => (gen_test!($name $test Export);)
+    }
+    test_helpers::codegen_tests!("*.wit");
 }
 
-#[rustfmt::skip]
 mod imports {
-    test_helpers::codegen_js_import!(
-        "*.wit"
-    );
+    macro_rules! codegen_test {
+        ($name:ident $test:tt) => (gen_test!($name $test Import);)
+    }
+    test_helpers::codegen_tests!("*.wit");
 }
 
-fn verify(dir: &str, name: &str) {
+fn verify(dir: &Path, name: &str) {
     let (cmd, args) = if cfg!(windows) {
         ("cmd.exe", &["/c", "npx.cmd"] as &[&str])
     } else {
         ("npx", &[] as &[&str])
     };
 
-    let status = Command::new(cmd)
-        .args(args)
-        .arg("eslint")
-        .arg("-c")
-        .arg(".eslintrc.js")
-        .arg(Path::new(dir).join(&format!("{}.js", name)))
-        .status()
-        .unwrap();
-    assert!(status.success());
+    test_helpers::run_command(
+        Command::new(cmd)
+            .args(args)
+            .arg("eslint")
+            .arg("-c")
+            .arg(".eslintrc.js")
+            .arg(dir.join(&format!("{}.js", name))),
+    );
 }