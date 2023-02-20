cb4461e72a8c5d330fa2318b61c618b949d1577b
Guy Bedford <gbedford@fastly.com>
2023-02-20 09:14:30 -0800

    fix: treat synckit as an optional dependency

--- packages/preview2-shim/lib/nodejs/http.js
+++ packages/preview2-shim/lib/nodejs/http.js
@@ -1,12 +1,13 @@
-import { createSyncFn } from "synckit";
-import path from "node:path";
+import { createRequire } from 'node:module';
 import { fileURLToPath } from "node:url";
 import { UnexpectedError } from "../http/error.js";
 
+const require = createRequire(fileURLToPath(import.meta.url));
+
 export function send(req) {
   console.log(`[http] Send (nodejs) ${req.uri}`);
-  const dirname = path.dirname(fileURLToPath(import.meta.url));
-  const syncFn = createSyncFn(path.resolve(dirname, "../http/make-request.js"));
+  const { createSyncFn } = require('synckit');
+  const syncFn = createSyncFn(fileURLToPath(new URL('../http/make-request.js'), import.meta.url));
   let rawResponse = syncFn(req);
   let response = JSON.parse(rawResponse);
   if (response.status) {
