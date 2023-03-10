886dd2c9641bdb4d7222cf2d078e83b6f9be1367
Eduardo Rodrigues <eduardomourar@users.noreply.github.com>
2023-03-10 01:12:45 +0100

    chore: update unit test

--- packages/preview2-shim/test/test.js
+++ packages/preview2-shim/test/test.js
@@ -2,7 +2,7 @@ import { ok } from 'node:assert';
 
 suite('Node.js', async () => {
   test('Basic importing', async () => {
-    const filesystem = await import('@bytecodealliance/preview2-shim/filesystem');
-    ok(filesystem.stat);
+    const { default: wasiImport } = await import('@bytecodealliance/preview2-shim');
+    ok(wasiImport['wasi-filesystem'].stat);
   });
 });
