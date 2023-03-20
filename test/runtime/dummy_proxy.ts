ad31089e6a112209ad10b6eea647a270f9a42c46
Eduardo Rodrigues <eduardomourar@users.noreply.github.com>
2023-03-20 21:17:55 +0100

    chore: use path alias in test tsconfig

--- test/runtime/dummy_proxy.ts
+++ test/runtime/dummy_proxy.ts
@@ -1,14 +1,14 @@
 // Flags: --instantiation
 
 import * as assert from "node:assert";
-import importObject from "../../packages/preview2-shim";
+import { importObject } from "@bytecodealliance/preview2-shim";
 import { instantiate } from "../output/dummy_proxy/dummy_proxy.js";
 import * as helpers from "./helpers.js";
 
 async function run() {
   const wasm = await instantiate(helpers.loadWasm, importObject);
 
-  const run = () => wasm.HTTP.handle({}, {});
+  const run = () => wasm.HTTP.handle(0, 1);
 
   assert.throws(run, Error(""));
 }
