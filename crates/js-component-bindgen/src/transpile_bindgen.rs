d3875000a46ca450714d85e418dd50966321d0a2
Guy Bedford <gbedford@fastly.com>
2023-08-03 13:59:52 -0700

    deps: use wit-component string encoding for public interface (#121)

--- crates/js-component-bindgen/src/transpile_bindgen.rs
+++ crates/js-component-bindgen/src/transpile_bindgen.rs
@@ -11,11 +11,15 @@ use indexmap::IndexMap;
 use std::collections::{BTreeMap, BTreeSet, HashMap};
 use std::fmt::Write;
 use std::mem;
-use wasmtime_environ::component::{
-    CanonicalOptions, Component, CoreDef, CoreExport, Export, ExportItem, GlobalInitializer,
-    InstantiateModule, LowerImport, RuntimeInstanceIndex, StaticModuleIndex, Transcoder,
+use wasmtime_environ::{
+    component,
+    component::{
+        CanonicalOptions, Component, CoreDef, CoreExport, Export, ExportItem, GlobalInitializer,
+        InstantiateModule, LowerImport, RuntimeInstanceIndex, StaticModuleIndex, Transcoder,
+    },
 };
 use wasmtime_environ::{EntityIndex, ModuleTranslation, PrimaryMap};
+use wit_component::StringEncoding;
 use wit_parser::abi::{AbiVariant, LiftLower};
 use wit_parser::*;
 
@@ -568,7 +572,11 @@ impl Instantiator<'_, '_> {
             tmp: 0,
             params,
             post_return: post_return.as_ref(),
-            encoding: opts.string_encoding,
+            encoding: match opts.string_encoding {
+                component::StringEncoding::Utf8 => StringEncoding::UTF8,
+                component::StringEncoding::Utf16 => StringEncoding::UTF16,
+                component::StringEncoding::CompactUtf16 => StringEncoding::CompactUTF16,
+            },
             src: source::Source::default(),
         };
         self.resolve.call(
