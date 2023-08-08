f1a512f07ebce42b83cab802beeaae9878f85466
Guy Bedford <gbedford@fastly.com>
2023-08-08 14:11:22 -0700

    fix: identifier naming bug (#126)

--- test/runtime.js
+++ test/runtime.js
@@ -1,17 +1,20 @@
 import { strictEqual } from 'node:assert';
 import { existsSync } from 'node:fs';
 import { exec } from './helpers.js';
+import { codegenRunning, codegenPromise } from './codegen.js';
 
 export async function runtimeTest (fixtures) {
-  suite('Runtime', () => {
-
+  suite('Runtime', async () => {
     for (const fixture of fixtures) {
       if (fixture.startsWith('dummy_')) continue;
-      const runtimeJs = fixture.replace('.component.wasm', '.js').replace('.wat', '.js');
-      if (!existsSync(`test/output/${runtimeJs}`))
+      const runtimeName = fixture.replace(/(\.component)?\.(wat|wasm)$/, '');
+      if (!existsSync(`test/runtime/${runtimeName}.ts`))
         continue;
-      test(runtimeJs, async () => {
-        const { stderr } = await exec(process.argv[0], `test/output/${runtimeJs}`);
+      test(runtimeName, async () => {
+        if (codegenRunning) {
+          await codegenPromise;
+        }
+        const { stderr } = await exec(process.argv[0], `test/output/${runtimeName}.js`);
         strictEqual(stderr, '');
       });
     }
