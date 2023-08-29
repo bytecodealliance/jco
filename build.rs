369bd6d2c9e21f2c69ca8399791322dd1496ab5b
Eduardo Rodrigues <eduardomourar@users.noreply.github.com>
2023-08-29 15:29:35 +0200

    chore: ensure wasi http proxy world is generated

--- build.rs
+++ build.rs
@@ -5,21 +5,24 @@ use js_component_bindgen::{generate_types, source::wit_parser::Resolve};
 
 fn main() -> Result<()> {
     if env::var("PREVIEW2_SHIM_TYPES").is_ok() {
-        for world in ["command"] {
-            let name = format!("wasi-{}", world);
+        for world in ["wasi:http/proxy", "wasi:cli/command"] {
+            let name = world.replace([':', '/'], "-");
             let preview2_wit_path = "./test/fixtures/wit";
 
             let mut resolve = Resolve::default();
             let (_, _) = resolve.push_dir(&PathBuf::from(preview2_wit_path))?;
 
+            let world_parts: Vec<&str> = world.split('/').collect();
+            let world_name = world_parts[1];
+
             let preview2 = *resolve
                 .package_names
                 .iter()
-                .find(|(name, _)| name.name == "preview")
+                .find(|(name, _)| name.interface_id(world_name) == world)
                 .unwrap()
                 .1;
 
-            let world = resolve.select_world(preview2, Some(world))?;
+            let world = resolve.select_world(preview2, Some(world_name))?;
 
             let opts = js_component_bindgen::TranspileOpts {
                 name: "component".to_string(),
