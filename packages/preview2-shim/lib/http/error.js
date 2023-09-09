f146d87bd7c9a076feff8d86708c2e30e592be10
Eduardo Rodrigues <eduardomourar@users.noreply.github.com>
2023-09-09 20:08:25 +0200

    chore: ensure http error have right shape

--- packages/preview2-shim/lib/http/error.js
+++ packages/preview2-shim/lib/http/error.js
@@ -1,11 +1,11 @@
+/**
+ * @implements { import("../../types/imports/wasi-http-types").ErrorUnexpectedError }
+ */
 export class UnexpectedError extends Error {
-  /** @type { import("../../types/imports/wasi-http-types").ErrorUnexpectedError } */
-  payload;
+  tag = "unexpected-error";
+  /** @type string */ val;
   constructor(message = "unexpected-error") {
     super(message);
-    this.payload = {
-      tag: "unexpected-error",
-      val: message,
-    };
+    this.val = message;
   }
 }
