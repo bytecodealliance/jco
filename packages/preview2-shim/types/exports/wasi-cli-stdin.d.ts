a0486b0c1c0794e290b21c2d1d99d7644d2bbf71
Guy Bedford <gbedford@fastly.com>
2023-09-19 12:31:49 -0700

    regenerate preview2 types

--- packages/preview2-shim/types/exports/wasi-cli-stdin.d.ts
+++
@@ -1,5 +0,0 @@
-export namespace WasiCliStdin {
-  export function getStdin(): InputStream;
-}
-import type { InputStream } from '../exports/wasi-io-streams';
-export { InputStream };
--- packages/preview2-shim/types/imports/wasi-cli-stdin.d.ts
+++
@@ -1,5 +0,0 @@
-export namespace WasiCliStdin {
-  export function getStdin(): InputStream;
-}
-import type { InputStream } from '../imports/wasi-io-streams';
-export { InputStream };
