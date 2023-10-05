5f4d77e836b1045fc2baaf53a4e5534e09a4f5fa
Yosh <github@yosh.is>
2023-10-05 20:44:24 +0200

    move `self_build` to xtask

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("hello from xtask");
+use structopt::StructOpt;
+
+mod self_build;
+
+#[derive(StructOpt)]
+enum Opts {
+    /// Build self
+    Build,
+}
+
+fn main() -> anyhow::Result<()> {
+    match Opts::from_args() {
+        Opts::Build => self_build::run(),
+    }
 }
