a7f39f68bab43c8452bf533b0d8efc9dfb1c8f0a
Yosh <github@yosh.is>
2023-10-06 14:30:45 +0200

    add `cargo xtask build`

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,15 +1,23 @@
 use structopt::StructOpt;
 
+mod build;
 mod self_build;
 
 #[derive(StructOpt)]
 enum Opts {
     /// Build the `jco` tools
     SelfBuild,
+    /// Build
+    Build,
 }
 
 fn main() -> anyhow::Result<()> {
     match Opts::from_args() {
         Opts::SelfBuild => self_build::run(),
+        Opts::Build => {
+            build::run()?;
+            self_build::run()?;
+            Ok(())
+        }
     }
 }
