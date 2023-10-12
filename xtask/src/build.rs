8ee1c5127dd1c4dc6e518ca15909911c49467df1
Guy Bedford <guybedford@gmail.com>
2023-10-11 19:03:31 -0700

    fmt

--- xtask/src/build.rs
+++ xtask/src/build.rs
@@ -4,7 +4,13 @@ pub(crate) fn run() -> anyhow::Result<()> {
     let sh = Shell::new()?;
     cmd!(sh, "cargo build --workspace --target wasm32-wasi --release").read()?;
     cmd!(sh, "cargo run").read()?;
-    sh.copy_file("lib/wasi_snapshot_preview1.command.wasm", "node_modules/@bytecodealliance/jco/lib")?;
-    sh.copy_file("lib/wasi_snapshot_preview1.command.wasm", "node_modules/@bytecodealliance/jco/lib")?;
+    sh.copy_file(
+        "lib/wasi_snapshot_preview1.command.wasm",
+        "node_modules/@bytecodealliance/jco/lib",
+    )?;
+    sh.copy_file(
+        "lib/wasi_snapshot_preview1.command.wasm",
+        "node_modules/@bytecodealliance/jco/lib",
+    )?;
     Ok(())
 }
