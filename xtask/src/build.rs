82146dc7a5e32a67a80969dae7238676862b61f8
Guy Bedford <gbedford@fastly.com>
2023-10-13 10:21:00 -0700

    fix: xtask weirdness

--- xtask/src/build.rs
+++ xtask/src/build.rs
@@ -3,13 +3,13 @@ use xshell::{cmd, Shell};
 pub(crate) fn run() -> anyhow::Result<()> {
     let sh = Shell::new()?;
     cmd!(sh, "cargo build --workspace --target wasm32-wasi --release").read()?;
-    sh.copy_file(
-        "lib/wasi_snapshot_preview1.command.wasm",
-        "node_modules/@bytecodealliance/jco/lib",
-    )?;
-    sh.copy_file(
-        "lib/wasi_snapshot_preview1.command.wasm",
-        "node_modules/@bytecodealliance/jco/lib",
-    )?;
+    // sh.copy_file(
+    //     "lib/wasi_snapshot_preview1.command.wasm",
+    //     "node_modules/@bytecodealliance/jco/lib",
+    // )?;
+    // sh.copy_file(
+    //     "lib/wasi_snapshot_preview1.reactor.wasm",
+    //     "node_modules/@bytecodealliance/jco/lib",
+    // )?;
     Ok(())
 }
