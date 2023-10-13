10961580ead966d4154a1bb3627453fc94878624
Yosh <github@yosh.is>
2023-10-13 16:18:00 +0200

    Update update-tests.sh

--- update-tests.sh
+++ update-tests.sh
@@ -1,5 +1,6 @@
-git clone https://github.com/bytecodealliance/wit-bindgen
-cd wit-bindgen
+git submodule foreach --recursive git update --remote
+
+cd submodules/wit-bindgen
 cargo test -p wit-bindgen-cli --no-default-features -F rust -F c
 
 for t in target/runtime-tests/*/rust.wasm
@@ -15,22 +16,17 @@ do
   name="$(basename $(dirname $t))"
   name=${name:2}
   echo "cp $t ../test/fixtures/components/${name}.component.wasm"
-  cp $t ../test/fixtures/components/${name}.component.wasm
+  cp $t ../../test/fixtures/components/${name}.component.wasm
 done
 
 # copy flavorful wit case
-cp tests/runtime/flavorful/world.wit ../test/fixtures/wit/flavorful.wit
-
-cd ..
+cp tests/runtime/flavorful/world.wit ../../test/fixtures/wit/flavorful.wit
+cd ../..
 
 ./src/jco.js componentize test/fixtures/component-gen/import-fn.js --wit test/fixtures/component-gen/import-fn.wit -o test/fixtures/components/import-fn.component.wasm
 
-rm -rf wit-bindgen
-
 ## wasi virt to generate composition case
-git clone https://github.com/bytecodealliance/wasi-virt
-cd wasi-virt
+cd submodules/wasi-virt
 cargo test
-cp tests/generated/env-allow.composed.wasm ../test/fixtures/
-cd ..
-rm -rf wasi-virt
+cp tests/generated/env-allow.composed.wasm ../../test/fixtures/
+cd ../..
