cc11b102180e7a6b0ec6a3ba314e1b721fa80fd9
Yosh <github@yosh.is>
2023-10-25 00:36:49 +0200

    Rename `xtask build shims` to `xtask generate wasi-types`

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -27,22 +27,21 @@ enum Update {
 enum Build {
     /// Build and transpile the `jco` tools
     Jco,
-    /// Build the shims
-    Shims,
     /// Build the project and copy the binaries
     Workspace,
 }
 
 #[derive(StructOpt)]
 enum Generate {
-    /// Generate tests
+    /// Generate WASI conformance tests from Wasmtime
     Tests,
+    /// Generate the WASI Preview 2 types
+    WasiTypes,
 }
 
 fn main() -> anyhow::Result<()> {
     match Opts::from_args() {
         Opts::Build(Build::Jco) => build::jco::run(),
-        Opts::Build(Build::Shims) => build::shims::run(),
         Opts::Build(Build::Workspace) => {
             build::workspace::run()?;
             build::jco::run()?;
@@ -51,5 +50,6 @@ fn main() -> anyhow::Result<()> {
         Opts::Test => test::run(),
         Opts::Update(Update::Preview2) => update::preview2::run(),
         Opts::Generate(Generate::Tests) => generate::tests::run(),
+        Opts::Generate(Generate::WasiTypes) => generate::wasi_types::run(),
     }
 }
