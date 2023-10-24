fdf579c0ddcd991f3e2a91617b14ad994b4b7378
Yosh <github@yosh.is>
2023-10-24 03:18:41 +0200

    first pass at test codegen

--- xtask/src/generate/tests.rs
+++ xtask/src/generate/tests.rs
@@ -9,6 +9,11 @@ pub fn run() -> anyhow::Result<()> {
     cmd!(sh, "cargo build --target wasm32-wasi --release").run()?;
     drop(guard);
 
+    // Make sure we have a dir to write to
+    fs::create_dir_all("./tests/generated")?;
+
+    let mut test_names = vec![];
+
     for entry in fs::read_dir("submodules/wasmtime/target/wasm32-wasi/release")? {
         let entry = entry?;
 
@@ -18,7 +23,12 @@ pub fn run() -> anyhow::Result<()> {
         }
 
         let path = entry.path();
+        let file_stem = path.file_stem();
         let path = match path.to_str() {
+            Some(path) => path,
+            None => continue,
+        };
+        let test_name = match file_stem.unwrap().to_str() {
             Some(path) => path,
             None => continue,
         };
@@ -28,8 +38,41 @@ pub fn run() -> anyhow::Result<()> {
             continue;
         }
 
-        cmd!(sh, "src/jco.js run {path}").run()?;
-        dbg!(entry);
+        test_names.push(test_name.to_owned());
+        let content = generate_test(test_name);
+        let file_name = format!("tests/generated/{test_name}.rs");
+        fs::write(&file_name, content)?;
+        eprintln!("wrote {file_name}");
     }
+
+    let content = generate_mod(test_names.as_slice());
+    let file_name = format!("tests/generated/mod.rs");
+    fs::write(&file_name, content)?;
+    eprintln!("wrote {file_name}");
     Ok(())
+}
+
+fn generate_test(test_name: &str) -> String {
+    format!(
+        r##"use tempdir::TempDir;
+use xshell::{{cmd, Shell}};
+
+#[test]
+fn {test_name}() -> anyhow::Result<()> {{
+    let sh = Shell::new()?;
+    let file_name = "{test_name}";
+    let tempdir = TempDir::new("{{file_name}}")?;
+    let wasi_file = test_utils::compile(&sh, &tempdir, &file_name)?;
+    cmd!(sh, "./src/jco.js run {{wasi_file}}").run()?;
+    Ok(())
+}}
+    "##
+    )
+}
+
+fn generate_mod(test_names: &[String]) -> String {
+    test_names
+        .into_iter()
+        .map(|t| format!("mod {t};\n"))
+        .collect()
 }
