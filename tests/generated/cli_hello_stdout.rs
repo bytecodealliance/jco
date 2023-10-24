f15bafd9023d629a9da4c186034fa2f0b51dc0bd
Yosh <github@yosh.is>
2023-10-24 03:28:08 +0200

    generate tests

--- tests/generated/cli_hello_stdout.rs
+++ tests/generated/cli_hello_stdout.rs
@@ -1,12 +1,16 @@
+//! This file has been auto-generated, please do not modify manually
+//! To regenerate this file re-run `cargo xtask generate tests` from the project root
+
 use tempdir::TempDir;
 use xshell::{cmd, Shell};
 
 #[test]
 fn cli_hello_stdout() -> anyhow::Result<()> {
     let sh = Shell::new()?;
-    let tempdir = TempDir::new("cli-hello-test")?;
     let file_name = "cli_hello_stdout";
+    let tempdir = TempDir::new("{file_name}")?;
     let wasi_file = test_utils::compile(&sh, &tempdir, &file_name)?;
     cmd!(sh, "./src/jco.js run {wasi_file}").run()?;
     Ok(())
 }
+    
