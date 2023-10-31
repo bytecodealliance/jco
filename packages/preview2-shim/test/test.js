81b3bb8ac40e83985f569cb2d92c925e56293691
Guy Bedford <gbedford@fastly.com>
2023-10-31 14:47:59 -0700

    local http test

--- packages/preview2-shim/test/test.js
+++ packages/preview2-shim/test/test.js
@@ -9,8 +9,8 @@ suite('Node.js Preview2', async () => {
     cli.stderr.getStderr().blockingWriteAndFlush(new TextEncoder().encode('test stderr'));
   });
 
-  test('Fs read', async () => {
-    const { filesystem, io } = await import('@bytecodealliance/preview2-shim');
+  test('FS read', async () => {
+    const { filesystem } = await import('@bytecodealliance/preview2-shim');
     const [[rootDescriptor]] = filesystem.preopens.getDirectories();
     const childDescriptor = rootDescriptor.openAt({}, fileURLToPath(import.meta.url), {}, {}, {});
     const stream = childDescriptor.readViaStream(0);
@@ -18,5 +18,51 @@ suite('Node.js Preview2', async () => {
     const source = new TextDecoder().decode(buf);
     ok(source.includes('UNIQUE STRING'));
     stream[Symbol.dispose]();
+  });
+
+  test('WASI HTTP', async () => {
+    const { http } = await import('@bytecodealliance/preview2-shim');
+    const { handle } = http.outgoingHandler;
+    const { OutgoingRequest, OutgoingBody, Fields } = http.types;
+    const encoder = new TextEncoder();
+    const request = new OutgoingRequest(
+      { tag: 'GET' },
+      '/',
+      { tag: 'HTTPS' },
+      'webassembly.org',
+      new Fields([
+        ['User-agent', encoder.encode('WASI-HTTP/0.0.1')],
+        ['Content-type', encoder.encode('application/json')],
+      ])
+    );
+
+    const outgoingBody = request.write();
+    // TODO: we should explicitly drop the bodyStream here
+    //       when we have support for Symbol.dispose
+    OutgoingBody.finish(outgoingBody);
+
+    const futureIncomingResponse = handle(request);
+    const incomingResponse = futureIncomingResponse.get().val.val;
+
+    const status = incomingResponse.status();
+    const responseHeaders = incomingResponse.headers().entries();
+
+    const decoder = new TextDecoder();
+    const headers = Object.fromEntries(responseHeaders.map(([k, v]) => [k, decoder.decode(v)]));
+
+    let responseBody;
+    const incomingBody = incomingResponse.consume();
+    {
+      const bodyStream = incomingBody.stream();
+      // const bodyStreamPollable = bodyStream.subscribe();
+      const buf = bodyStream.read(50n);
+      // TODO: actual streaming
+      // TODO: explicit drops
+      responseBody = buf.length > 0 ? new TextDecoder().decode(buf) : undefined;
+    }
+
+    strictEqual(status, 200);
+    ok(headers['content-type'].startsWith('text/html'));
+    ok(responseBody.includes('WebAssembly'));
   });
 });
