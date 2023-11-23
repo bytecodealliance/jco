7d4461553dfe4ef3473e0378f0be87057f9f1f39
Ivan Enderlin <ivan@mnt.io>
2023-11-23 10:08:40 +0100

    fix: Ensure that the `export` for `instantiate` is removed.
    `--js` rewrites `export async function instantiate` into `async
    function instantiate`. Good. But now there is `--instantiation=sync`, so
    `instantiate` can be non-async.
    
    This patch handles `export async function instantiate` but also `export
    function instantiate`, and rewrite it without the `export ` prefix.

--- src/cmd/transpile.js
+++ src/cmd/transpile.js
@@ -180,7 +180,7 @@ export async function transpileComponent (component, opts = {}) {
 
       const outSource = `${
         imports.map((impt, i) => `import * as import${i} from '${impt}';`).join('\n')}
-${source.replace('export async function instantiate', 'async function instantiate')}
+${source.replace(/export (async )?function instantiate/, '$1function instantiate')}
 
 let ${exports.filter(([, ty]) => ty === 'function').map(([name]) => '_' + name).join(', ')};
 
