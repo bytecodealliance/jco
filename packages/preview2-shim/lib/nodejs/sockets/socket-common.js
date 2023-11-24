9f77ac161bb14ed81305822e30248daa864e866d
Wassim Chegham <github@wassim.dev>
2023-11-24 12:27:10 +0100

    fix: refactor isUnicastIpAddress

--- packages/preview2-shim/lib/nodejs/sockets/socket-common.js
+++ packages/preview2-shim/lib/nodejs/sockets/socket-common.js
@@ -67,14 +67,17 @@ export function deserializeIpAddress(addr, family) {
 }
 
 export function isUnicastIpAddress(ipSocketAddress) {
-  return ipSocketAddress.val.address[0] !== 0xff; // 255
+  return !isMulticastIpAddress(ipSocketAddress) && !isBroadcastIpAddress(ipSocketAddress);
 }
 
 export function isMulticastIpAddress(ipSocketAddress) {
+  // ipv6: [0xff00, 0, 0, 0, 0, 0, 0, 0]
+  // ipv4: [224, 0, 0, 0]
   return ipSocketAddress.val.address[0] === 224 || ipSocketAddress.val.address[0] === 0xff00;
 }
 
 export function isBroadcastIpAddress(ipSocketAddress) {
+  // ipv4: [255, 255, 255, 255]
   return (
     ipSocketAddress.val.address[0] === 0xff && // 255
     ipSocketAddress.val.address[1] === 0xff && // 255
