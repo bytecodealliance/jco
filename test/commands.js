7ddbf62d454a18a296dde180981a873158d1eb39
Guy Bedford <gbedford@fastly.com>
2024-05-01 13:38:33 -0700

    fixup commands test structure

--- test/commands.js
+++ test/commands.js
@@ -1,26 +1,24 @@
 import { strictEqual } from "node:assert";
-import { readdirSync } from "node:fs";
+import { readdirSync, readFileSync } from "node:fs";
 import { resolve } from "node:path";
 import { exec, jcoPath } from "./helpers.js";
 
-const args = {
-  "dir.virt": ["/foo"],
-};
+const tests = JSON.parse(readFileSync('test/fixtures/commands/tests.json', 'utf8'));
 
 export async function commandsTest() {
   suite("Commands", async () => {
     for (const fixture of readdirSync("test/fixtures/commands")) {
+      if (fixture === 'tests.json') continue;
       const runName = fixture.replace(/\.(wat|wasm)$/, "");
       test(runName, async () => {
         const { stdout, stderr } = await exec(
           jcoPath,
           "run",
           resolve("test/fixtures/commands", fixture),
-          ...(args[runName] || [])
+          ...(tests[runName]?.args || [])
         );
-
-        strictEqual(stderr, "");
-        strictEqual(stdout, "Listing directory: /foo\n - .\n - ..\n - foo\n");
+        strictEqual(stderr, tests[runName]?.stderr || '');
+        strictEqual(stdout, tests[runName]?.stdout || '');
       });
     }
   });
