8abf4122b9587c3263f538509e660599494b8378
Guy Bedford <gbedford@fastly.com>
2024-05-06 11:51:04 -0700

    1.2.4

--- src/jco.js
+++ src/jco.js
@@ -11,7 +11,7 @@ program
   .name('jco')
   .description(c`{bold jco - WebAssembly JS Component Tools}\n      JS Component Transpilation Bindgen & Wasm Tools for JS`)
   .usage('<command> [options]')
-  .version('1.2.3');
+  .version('1.2.4');
 
 function myParseInt(value) {
   return parseInt(value, 10);
