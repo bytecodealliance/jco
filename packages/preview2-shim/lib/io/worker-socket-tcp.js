19c4d91a6ef37e96dba3d0c9b187f41eebe0842d
Guy Bedford <gbedford@fastly.com>
2024-08-20 11:36:00 -0700

    remove unused

--- packages/preview2-shim/lib/io/worker-socket-tcp.js
+++ packages/preview2-shim/lib/io/worker-socket-tcp.js
@@ -10,7 +10,6 @@ import {
 } from "./worker-thread.js";
 import process from "node:process";
 const { TCP, constants: TCPConstants } = process.binding("tcp_wrap");
-const { ShutdownWrap } = process.binding('stream_wrap');
 import {
   convertSocketError,
   convertSocketErrorCode,
