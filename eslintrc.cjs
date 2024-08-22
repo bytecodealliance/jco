bf49e3279350cdeb515a048b9edaa7b0dec4dc5b
Guy Bedford <gbedford@fastly.com>
2024-08-21 17:44:37 -0700

    deps: update to latest deps, including ComponentizeJS

--- eslintrc.cjs
+++ eslintrc.cjs
@@ -1,14 +1,4 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 13,
-        "sourceType": "module"
-    },
     "rules": {
       "no-sparse-arrays": 0,
       "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
--- test/eslintrc.cjs
+++ test/eslintrc.cjs
@@ -1,14 +1,4 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 13,
-        "sourceType": "module"
-    },
     "rules": {
       // allow this since we generate `const {} = e;` for empty structs
       "no-empty-pattern": 0,
