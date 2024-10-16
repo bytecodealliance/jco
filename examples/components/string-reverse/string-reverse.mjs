8f776c3f9e23596fa01ba69382d2b45ffbcb83cd
Victor Adossi <vadossi@cosmonic.com>
2024-10-16 17:19:59 +0900

    refactor(examples): inline reverseString() in example
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- examples/components/string-reverse/string-reverse.mjs
+++ examples/components/string-reverse/string-reverse.mjs
@@ -3,19 +3,18 @@
  */
 
 /**
- * This Javascript will be interpreted by `jco` and turned into a
- * WebAssembly binary with a single export (this `reverse` function).
- */
-function reverseString(s) {
-  return s.split("")
-    .reverse()
-    .join("");
-}
-
-/**
  * The Javascript export below represents the export of the `reverse` interface,
  * which which contains `reverse-string` as it's primary exported function.
  */
 export const reverse = {
-  reverseString,
+  /**
+   * This Javascript will be interpreted by `jco` and turned into a
+   * WebAssembly binary with a single export (this `reverse` function).
+   */
+  reverseString(s) {
+    return s.split("")
+      .reverse()
+      .join("");
+  }
+
 };
