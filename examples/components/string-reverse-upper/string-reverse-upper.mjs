8ba4fe951cb2f6db3ecca46916640ce24c317111
Victor Adossi <vadossi@cosmonic.com>
2024-10-16 17:18:32 +0900

    refactor(examples): inline reverseAndUppercase() in component
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- examples/components/string-reverse-upper/string-reverse-upper.mjs
+++ examples/components/string-reverse-upper/string-reverse-upper.mjs
@@ -11,18 +11,16 @@
 import { reverseString } from 'example:string-reverse/reverse@0.1.0';
 
 /**
- * Represents the implementation of the `reverse-and-uppercase` function in the `reversed-upper` interface
- *
- * This function makes use of `reverse-string` which is *imported* from another WebAssembly binary.
- */
-function reverseAndUppercase(s) {
-  return reverseString(s).toLocaleUpperCase();
-}
-
-/**
  * The Javascript export below represents the export of the `reversed-upper` interface,
  * which which contains `revup` as it's primary exported function.
  */
 export const reversedUpper = {
-  reverseAndUppercase,
+  /**
+   * Represents the implementation of the `reverse-and-uppercase` function in the `reversed-upper` interface
+   *
+   * This function makes use of `reverse-string` which is *imported* from another WebAssembly binary.
+   */
+  reverseAndUppercase() {
+    return reverseString(s).toLocaleUpperCase();
+  },
 };
