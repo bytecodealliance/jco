f218fef183824c3e939a7bef3ad8e6ad96ca1661
Calvin Prewitt <calvin@jaflabs.com>
2024-11-03 18:30:41 -0600

    starting to setup wasi shims for async

---
+++ packages/preview2-shim/lib/browser-async/index.js
@@ -0,0 +1,17 @@
+import * as clocks from "./clocks.js";
+import * as filesystem from "./filesystem.js";
+import * as http from "./http.js";
+import * as io from "./io.js";
+import * as random from "./random.js";
+import * as sockets from "./sockets.js";
+import * as cli from "./cli.js";
+
+export {
+  clocks,
+  filesystem,
+  http,
+  io,
+  random,
+  sockets,
+  cli,
+}
