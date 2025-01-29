dbd77b83a1c9e4097f2f5370e062234453e5889c
Guy Bedford <gbedford@fastly.com>
2025-01-29 13:39:54 -0800

    fix: ensure optimize component works on core modules only, fix ranges, support noVerify

--- src/cmd/opt.js
+++ src/cmd/opt.js
@@ -1,16 +1,23 @@
-import { $init, tools } from '../../obj/wasm-tools.js';
+import { $init, tools } from "../../obj/wasm-tools.js";
 const { metadataShow, print } = tools;
-import { writeFile } from 'fs/promises';
-import { fileURLToPath } from 'url';
-import c from 'chalk-template';
-import { readFile, sizeStr, fixedDigitDisplay, table, spawnIOTmp, setShowSpinner, getShowSpinner } from '../common.js';
-import ora from '#ora';
+import { writeFile } from "fs/promises";
+import { fileURLToPath } from "url";
+import c from "chalk-template";
+import {
+  readFile,
+  sizeStr,
+  fixedDigitDisplay,
+  table,
+  spawnIOTmp,
+  setShowSpinner,
+  getShowSpinner,
+} from "../common.js";
+import ora from "#ora";
 
-export async function opt (componentPath, opts, program) {
+export async function opt(componentPath, opts, program) {
   await $init;
-  const varIdx = program.parent.rawArgs.indexOf('--');
-  if (varIdx !== -1)
-    opts.optArgs = program.parent.rawArgs.slice(varIdx + 1);
+  const varIdx = program.parent.rawArgs.indexOf("--");
+  if (varIdx !== -1) opts.optArgs = program.parent.rawArgs.slice(varIdx + 1);
   const componentBytes = await readFile(componentPath);
 
   if (!opts.quiet) setShowSpinner(true);
@@ -19,80 +26,102 @@ export async function opt (componentPath, opts, program) {
 
   await writeFile(opts.output, component);
 
-  let totalBeforeBytes = 0, totalAfterBytes = 0;
+  let totalBeforeBytes = 0,
+    totalAfterBytes = 0;
 
   if (!opts.quiet)
     console.log(c`
 {bold Optimized WebAssembly Component Internal Core Modules:}
 
-${table([...compressionInfo.map(({ beforeBytes, afterBytes }, i) => {
-  totalBeforeBytes += beforeBytes;
-  totalAfterBytes += afterBytes;
-  return [
-    ` - Core Module ${i + 1}:  `,
-    sizeStr(beforeBytes),
-    ' -> ',
-    c`{cyan ${sizeStr(afterBytes)}} `,
-    `(${fixedDigitDisplay(afterBytes / beforeBytes * 100, 2)}%)`
-  ];
-}), ['', '', '', '', ''], [
-  ` = Total:  `,
-  `${sizeStr(totalBeforeBytes)}`,
-  ` => `,
-  c`{cyan ${sizeStr(totalAfterBytes)}} `,
-  `(${fixedDigitDisplay(totalAfterBytes / totalBeforeBytes * 100, 2)}%)`
-]], [,,,,'right'])}`);
+${table(
+  [
+    ...compressionInfo.map(({ beforeBytes, afterBytes }, i) => {
+      totalBeforeBytes += beforeBytes;
+      totalAfterBytes += afterBytes;
+      return [
+        ` - Core Module ${i + 1}:  `,
+        sizeStr(beforeBytes),
+        " -> ",
+        c`{cyan ${sizeStr(afterBytes)}} `,
+        `(${fixedDigitDisplay((afterBytes / beforeBytes) * 100, 2)}%)`,
+      ];
+    }),
+    ["", "", "", "", ""],
+    [
+      ` = Total:  `,
+      `${sizeStr(totalBeforeBytes)}`,
+      ` => `,
+      c`{cyan ${sizeStr(totalAfterBytes)}} `,
+      `(${fixedDigitDisplay((totalAfterBytes / totalBeforeBytes) * 100, 2)}%)`,
+    ],
+  ],
+  [, , , , "right"]
+)}`);
 }
 
 /**
- * 
- * @param {Uint8Array} componentBytes 
- * @param {{ quiet: boolean, optArgs?: string[] }} options?
+ *
+ * @param {Uint8Array} componentBytes
+ * @param {{ quiet: boolean, optArgs?: string[], noVerify?: boolean }} options?
  * @returns {Promise<{ component: Uint8Array, compressionInfo: { beforeBytes: number, afterBytes: number }[] >}
  */
-export async function optimizeComponent (componentBytes, opts) {
+export async function optimizeComponent(componentBytes, opts) {
   await $init;
   const showSpinner = getShowSpinner();
   let spinner;
   try {
-    const coreModules = metadataShow(componentBytes).slice(1, -1).map(({ range }) => range);
+    const coreModules = metadataShow(componentBytes)
+      .slice(1, -1)
+      .filter(({ isComponent }) => !isComponent)
+      .map(({ range }) => range);
 
     let completed = 0;
-    const spinnerText = () => c`{cyan ${completed} / ${coreModules.length}} Running Binaryen on WebAssembly Component Internal Core Modules \n`;
+    const spinnerText = () =>
+      c`{cyan ${completed} / ${coreModules.length}} Running Binaryen on WebAssembly Component Internal Core Modules \n`;
     if (showSpinner) {
       spinner = ora({
-        color: 'cyan',
-        spinner: 'bouncingBar'
+        color: "cyan",
+        spinner: "bouncingBar",
       }).start();
       spinner.text = spinnerText();
     }
 
-    const optimizedCoreModules = await Promise.all(coreModules.map(async ([coreModuleStart, coreModuleEnd]) => {
-      const optimized = wasmOpt(componentBytes.subarray(coreModuleStart, coreModuleEnd), opts?.optArgs);
-      if (spinner) {
-        completed++;
-        spinner.text = spinnerText();
-      }
-      return optimized;
-    }));
+    const optimizedCoreModules = await Promise.all(
+      coreModules.map(async ([coreModuleStart, coreModuleEnd]) => {
+        const optimized = wasmOpt(
+          componentBytes.subarray(coreModuleStart, coreModuleEnd),
+          opts?.optArgs
+        );
+        if (spinner) {
+          completed++;
+          spinner.text = spinnerText();
+        }
+        return optimized;
+      })
+    );
 
     let outComponentBytes = new Uint8Array(componentBytes.byteLength);
-    let nextReadPos = 0, nextWritePos = 0;
+    let nextReadPos = 0,
+      nextWritePos = 0;
     for (let i = 0; i < coreModules.length; i++) {
       const [coreModuleStart, coreModuleEnd] = coreModules[i];
       const optimizedCoreModule = optimizedCoreModules[i];
 
       let lebByteLen = 1;
-      while (componentBytes[coreModuleStart - 1 - lebByteLen] & 0x80) lebByteLen++;
+      while (componentBytes[coreModuleStart - 1 - lebByteLen] & 0x80)
+        lebByteLen++;
 
       // Write from the last read to the LEB byte start of the core module
-      outComponentBytes.set(componentBytes.subarray(nextReadPos, coreModuleStart - lebByteLen), nextWritePos);
+      outComponentBytes.set(
+        componentBytes.subarray(nextReadPos, coreModuleStart - lebByteLen),
+        nextWritePos
+      );
       nextWritePos += coreModuleStart - lebByteLen - nextReadPos;
 
       // Write the new LEB bytes
       let val = optimizedCoreModule.byteLength;
       do {
-        const byte = val & 0x7F;
+        const byte = val & 0x7f;
         val >>>= 7;
         outComponentBytes[nextWritePos++] = val === 0 ? byte : byte | 0x80;
       } while (val !== 0);
@@ -104,43 +133,51 @@ export async function optimizeComponent (componentBytes, opts) {
       nextReadPos = coreModuleEnd;
     }
 
-    outComponentBytes.set(componentBytes.subarray(nextReadPos, componentBytes.byteLength), nextWritePos);
+    outComponentBytes.set(
+      componentBytes.subarray(nextReadPos, componentBytes.byteLength),
+      nextWritePos
+    );
     nextWritePos += componentBytes.byteLength - nextReadPos;
-    nextReadPos += componentBytes.byteLength - nextReadPos;
 
-    outComponentBytes = outComponentBytes.subarray(0, outComponentBytes.length + nextWritePos - nextReadPos);
+    outComponentBytes = outComponentBytes.subarray(0, nextWritePos);
 
     // verify it still parses ok
-    try {
-      await print(outComponentBytes);
-    } catch (e) {
-      throw new Error(`Internal error performing optimization.\n${e.message}`)
+    if (!noVerify) {
+      try {
+        await print(outComponentBytes);
+      } catch (e) {
+        throw new Error(`Internal error performing optimization.\n${e.message}`);
+      }
     }
 
     return {
       component: outComponentBytes,
-      compressionInfo: coreModules.map(([s, e], i) => ({ beforeBytes: e - s, afterBytes: optimizedCoreModules[i].byteLength }))
+      compressionInfo: coreModules.map(([s, e], i) => ({
+        beforeBytes: e - s,
+        afterBytes: optimizedCoreModules[i].byteLength,
+      })),
     };
-  }
-  finally {
-    if (spinner)
-      spinner.stop();
+  } finally {
+    if (spinner) spinner.stop();
   }
 }
 
 /**
- * @param {Uint8Array} source 
+ * @param {Uint8Array} source
  * @returns {Promise<Uint8Array>}
  */
-async function wasmOpt(source, args = ['-O1', '--low-memory-unused', '--enable-bulk-memory']) {
-  const wasmOptPath = fileURLToPath(import.meta.resolve('binaryen/bin/wasm-opt'));
+async function wasmOpt(
+  source,
+  args = ["-O1", "--low-memory-unused", "--enable-bulk-memory"]
+) {
+  const wasmOptPath = fileURLToPath(
+    import.meta.resolve("binaryen/bin/wasm-opt")
+  );
 
   try {
-    return await spawnIOTmp(wasmOptPath, source, [
-      ...args, '-o'
-    ]);
+    return await spawnIOTmp(wasmOptPath, source, [...args, "-o"]);
   } catch (e) {
-    if (e.toString().includes('BasicBlock requested'))
+    if (e.toString().includes("BasicBlock requested"))
       return wasmOpt(source, args);
     throw e;
   }
