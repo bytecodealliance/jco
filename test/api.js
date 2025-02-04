9d4a44fb9d803a2072b872f5cb42008e864da9fa
Victor Adossi <vadossi@cosmonic.com>
2025-02-04 20:10:08 +0900

    fix(test): wit component versions in tests
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- test/api.js
+++ test/api.js
@@ -166,7 +166,7 @@ export async function apiTest(_fixtures) {
         [
           "processed-by",
           [
-            ["wit-component", "0.220.0"],
+            ["wit-component", "0.224.0"],
             ["dummy-gen", "test"],
           ],
         ],
@@ -207,7 +207,7 @@ export async function apiTest(_fixtures) {
         [
           "processed-by",
           [
-            ["wit-component", "0.220.0"],
+            ["wit-component", "0.224.0"],
             ["dummy-gen", "test"],
           ],
         ],
