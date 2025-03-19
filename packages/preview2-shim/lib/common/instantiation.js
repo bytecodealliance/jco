e1ecd6883d7d4cde1f43c4a47d3ba28592332d07
Victor Adossi <vadossi@cosmonic.com>
2025-03-20 04:04:03 +0900

    refactor(p2-shim): remove imports helper
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- packages/preview2-shim/lib/common/instantiation.js
+++ packages/preview2-shim/lib/common/instantiation.js
@@ -5,6 +5,45 @@ import * as wasi from "@bytecodealliance/preview2-shim";
  * an instantiation of a WebAssembly component transpiled with jco
  * (i.e. via `jco transpile`).
  *
+ * Normally, transpiled components contain mapping for WASI interfaces
+ * and/or imports that import the relevant packages (ex. `@bytecodealliance/preview2-shim/clocks`)
+ * from the right sources.
+ *
+ * This function makes use of the `Shim` object to provide an object that can be easily
+ * fed to the `instantiate` function produced by a transpiled component:
+ *
+ * ```js
+ * import { Shim } from "@bytecodealliance/preview2-shim/instantiation"
+ * // ...
+ * import { instantiate } from "path/to/transpiled/component.js"
+ * // ...
+ * const component = await instantiate(null, new Shim().importObject())
+ * ```
+ *
+ * You can also replace imports that you'd like to override with custom implementations,
+ * by using the `Shim` object directly:
+ *
+ * ```js
+ * import { random } from "@bytecodealliance/preview2-shim"
+ * import { Shim } from "@bytecodealliance/preview2-shim/instantiation"
+ * // ...
+ * import { instantiate } from "path/to/transpiled/component.js"
+ * // ...
+ * const customShim = new Shim({
+ *     random: {
+ *         // For these two interfaces we re-use the default provided shim
+ *         random: random.random,
+ *         insecure-seed: random.insecureSeed,
+ *         // For insecure, we can supply our own custom implementation
+ *         insecure: {
+ *             ...
+ *         }
+ *     }
+ * });
+ *
+ * const component = await instantiate(null, customShim.importObject())
+ * ```
+ *
  * @class Shim
  */
 export class Shim {
@@ -74,49 +113,4 @@ export class Shim {
       "wasi:clocks/wall-clock": this.#clocks.wallClock,
     };
   }
-}
-
-/**
- * Generate a WASI imports object that can be used for
- * manual component instantiation of a transpiled component.
- *
- * Normally, transpiled components contain mapping for WASI interfaces
- * and/or imports that import the relevant packages (ex. `@bytecodealliance/preview2-shim/clocks`)
- * from the right sources.
- *
- * This function makes use of the `Shim` object to provide an object that can be easily
- * fed to the `instantiate` function produced by a transpiled component:
- *
- * ```js
- * import { imports } from "@bytecodealliance/preview2-shim/instantiation"
- * // ...
- * import { instantiate } from "path/to/transpiled/component.js"
- * // ...
- * const component = instantiate(null, imports())
- * ```
- *
- * You can also replace imports that you'd like to override with custom implementations,
- * by using the `Shim` object directly:
- *
- * ```js
- * import { imports, Shim } from "@bytecodealliance/preview2-shim/instantiation"
- * // ...
- * import { instantiate } from "path/to/transpiled/component.js"
- * // ...
- * const defaultImports = imports();
- * const customImports = new Shim({
- *     random: {
- *         random: defaultImports["wasi:random/random"],
- *         insecure: {
- *             // Your custom implementation for the `wasi:random/insecure` interface goes here
- *         }
- *         insecure-seed: defaultImports["wasi:random/insecure-seed"],
- *     }
- * });
- *
- * const component = instantiate(null, customImports.importObject())
- * ```
- */
-export function imports() {
-  return new Shim().importObject();
 }
