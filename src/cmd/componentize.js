e01ca1c8309534d6b2f1e3fb89a5104f2bcc2814
Victor Adossi <vadossi@cosmonic.com>
2025-03-20 20:40:22 +0900

    feat: add min stack size setting for AOT
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- src/cmd/componentize.js
+++ src/cmd/componentize.js
@@ -1,15 +1,20 @@
-import { readFile, writeFile } from 'node:fs/promises';
-import { resolve, basename } from 'node:path';
-import c from 'chalk-template';
+import { freemem } from "node:os";
+import { readFile, writeFile } from "node:fs/promises";
+import { resolve, basename } from "node:path";
 
-export async function componentize (jsSource, opts) {
-  const { componentize: componentizeFn } = await eval('import("@bytecodealliance/componentize-js")');
-  if (opts.disable?.includes('all')) {
-    opts.disable = ['stdio', 'random', 'clocks', 'http'];
+import c from "chalk-template";
+
+export async function componentize(jsSource, opts) {
+  const { componentize: componentizeFn } = await eval(
+    'import("@bytecodealliance/componentize-js")',
+  );
+  if (opts.disable?.includes("all")) {
+    opts.disable = ["stdio", "random", "clocks", "http"];
   }
-  const source = await readFile(jsSource, 'utf8');
+  const source = await readFile(jsSource, "utf8");
   const { component } = await componentizeFn(source, {
     enableAot: opts.aot,
+    aotMinStackSizeBytes: opts.aotMinStackSizeBytes ?? defaultMinStackSize(),
     wevalBin: opts.wevalBin,
     sourceName: basename(jsSource),
     witPath: resolve(opts.wit),
@@ -20,4 +25,15 @@ export async function componentize (jsSource, opts) {
   });
   await writeFile(opts.out, component);
   console.log(c`{green OK} Successfully written {bold ${opts.out}}.`);
+}
+
+/**
+ * Calculate the min stack size depending on free memory
+ *
+ * @param {number} freeMemoryBytes - Amount of free memory in the system, in bytes  (if not provided, os.freemem() is used)
+ * @returns {number} The minimum stack size that should be used as a default.
+ */
+function defaultMinStackSize(freeMemoryBytes) {
+  freeMemoryBytes = freeMemoryBytes ?? freemem();
+  return Math.max(8 * 1024 * 1024, Math.floor(freeMemoryBytes * 0.1));
 }
