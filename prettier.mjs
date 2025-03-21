18ccd80250286d9632cb08ff0a18401d6f8351ec
Victor Adossi <vadossi@cosmonic.com>
2025-03-21 16:16:32 +0900

    chore: add prettier config
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

---
+++ prettier.mjs
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+};
+
+export default config;
