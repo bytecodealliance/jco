e20c3a36117275547f90449828a5026c11b7ea84
Tomasz Andrzejak <andreiltd@gmail.com>
2025-05-08 15:47:14 +0200

    Add TCP sockets draft

--- packages/preview3-shim/lib/nodejs/index.js
+++ packages/preview3-shim/lib/nodejs/index.js
@@ -2,6 +2,7 @@ import * as cli from "./cli.js";
 import * as clocks from "./clocks.js";
 import * as filesystem from "./filesystem.js";
 import * as random from "./random.js";
+import * as sockets from "./sockets.js";
 import * as stream from "./stream.js";
 
-export { cli, clocks, filesystem, random, stream };
+export { cli, clocks, filesystem, random, sockets, stream };
