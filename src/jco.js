afa6853dfdc10a3fe63c770f767cdf4b56b50e0a
vados-cosmonic <123968127+vados-cosmonic@users.noreply.github.com>
2025-05-12 08:57:53 +0000

    release: jco v1.11.2
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/jco.js
+++ src/jco.js
@@ -13,7 +13,7 @@ program
   .name('jco')
   .description(c`{bold jco - WebAssembly JS Component Tools}\n      JS Component Transpilation Bindgen & Wasm Tools for JS`)
   .usage('<command> [options]')
-  .version('1.11.2-rc.2');
+  .version('1.11.2');
 
 function myParseInt(value) {
   return parseInt(value, 10);
