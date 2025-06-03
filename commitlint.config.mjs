082c443937f9359431c997120f0a40d89418dba3
Victor Adossi <vadossi@cosmonic.com>
2025-06-03 10:51:31 +0900

    fix: eslint and prettier config
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,30 +1,34 @@
 const config = {
-  extends: ["@commitlint/config-conventional"],
-  parserPreset: "conventional-changelog-conventionalcommits",
-  rules: {
-    "type-enum": [
-      2,
-      "always",
-      [
-        "build",
-        "chore",
-        "ci",
-        "debug",
-        "docs",
-        "feat",
-        "fix",
-        "perf",
-        "refactor",
-        "release",
-        "revert",
-        "style",
-        "test",
-      ],
-    ],
-    "scope-enum": [1, "always", ["jco", "bindgen", "p2-shim", "deps", "ci"]],
-    "scope-case": [2, "always", "lower-case"],
-    "body-max-line-length": [1, "always", 100],
-  },
+    extends: ['@commitlint/config-conventional'],
+    parserPreset: 'conventional-changelog-conventionalcommits',
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            [
+                'build',
+                'chore',
+                'ci',
+                'debug',
+                'docs',
+                'feat',
+                'fix',
+                'perf',
+                'refactor',
+                'release',
+                'revert',
+                'style',
+                'test',
+            ],
+        ],
+        'scope-enum': [
+            1,
+            'always',
+            ['jco', 'bindgen', 'p2-shim', 'deps', 'ci'],
+        ],
+        'scope-case': [2, 'always', 'lower-case'],
+        'body-max-line-length': [1, 'always', 100],
+    },
 };
 
 export default config;
---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import prettier from 'eslint-plugin-prettier';
+import { default as prettierConfig } from './prettier.mjs';
+
+const config = defineConfig([
+    globalIgnores(['test/output/**/*.js']),
+    {
+        plugins: {
+            prettier,
+        },
+        rules: {
+            'no-sparse-arrays': 0,
+            'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+            'no-fallthrough': 0,
+            'no-constant-condition': 0,
+            'prettier/prettier': ['error', prettierConfig],
+        },
+    },
+]);
+
+export default config;
