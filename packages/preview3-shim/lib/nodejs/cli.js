56f92f9a8bd253a483f8add1efa2547a8296ece0
Tomasz Andrzejak <andreiltd@gmail.com>
2025-06-05 17:53:40 +0200

    feat(p3-shim): impelement wasi preview3 cli API

---
+++ packages/preview3-shim/lib/nodejs/cli.js
@@ -0,0 +1,77 @@
+import process from 'node:process';
+import { Readable } from 'node:stream';
+
+import { ResourceWorker } from './workers/resource-worker.js';
+import { StreamReader } from './stream.js';
+
+export {
+    _appendEnv,
+    _setEnv,
+    _setArgs,
+    _setCwd,
+    _setTerminalStdin,
+    _setTerminalStdout,
+    _setTerminalStderr,
+    environment,
+    exit,
+    terminalInput,
+    terminalOutput,
+    terminalStdin,
+    terminalStdout,
+    terminalStderr,
+} from '@bytecodealliance/preview2-shim/cli';
+
+const _worker = new ResourceWorker(
+    new URL('./workers/cli-worker.js', import.meta.url)
+);
+
+export const stdin = {
+    /**
+     * Creates a StreamReader that reads from standard input.
+     *
+     * WIT:
+     * ```
+     * get-stdin: func() -> stream<u8>;
+     * ```
+     *
+     * @returns {StreamReader} A reader for the process standard input.
+     */
+    getStdin() {
+        const stream = Readable.toWeb(process.stdin);
+        return new StreamReader(stream);
+    },
+};
+
+export const stdout = {
+    /**
+     * Pipes the output of a StreamReader to standard output.
+     *
+     * WIT:
+     * ```
+     * set-stdout: func(data: stream<u8>);
+     * ```
+     *
+     * @param {StreamReader} streamReader - The reader whose output will be written to stdout.
+     */
+    setStdout(streamReader) {
+        const stream = streamReader.intoReadableStream();
+        _worker.run({ op: 'stdout', stream }, [stream]);
+    },
+};
+
+export const stderr = {
+    /**
+     * Pipes the output of a StreamReader to standard error.
+     *
+     * WIT:
+     * ```
+     * set-stderr: func(data: stream<u8>);
+     * ```
+     *
+     * @param {StreamReader} streamReader - The reader whose output will be written to stderr.
+     */
+    setStdout(streamReader) {
+        const stream = streamReader.intoReadableStream();
+        _worker.run({ op: 'stderr', stream }, [stream]);
+    },
+};
--- packages/preview3-shim/lib/nodejs/index.js
+++ packages/preview3-shim/lib/nodejs/index.js
@@ -1,3 +1,4 @@
+export * as cli from './cli.js';
 export * as filesystem from './filesystem.js';
-export * as stream from './stream.js';
 export * as future from './future.js';
+export * as stream from './stream.js';
