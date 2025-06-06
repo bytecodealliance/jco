8fae7fd824b9fd4efc9c0bc85ac33d5f7ead6137
Tomasz Andrzejak <andreiltd@users.noreply.github.com>
2025-06-06 09:52:20 +0200

    feat(p3-shim): implement wasi preview3 cli API (#731)
    * feat(p3-shim): impelement wasi preview3 cli API
    
    * fix: avoid side effects when loading cli script

--- packages/preview3-shim/lib/nodejs/index.js
+++ packages/preview3-shim/lib/nodejs/index.js
@@ -1,5 +1,6 @@
+export * as cli from './cli.js';
 export * as clocks from './clocks.js';
 export * as filesystem from './filesystem.js';
-export * as stream from './stream.js';
 export * as future from './future.js';
 export * as random from './random.js';
+export * as stream from './stream.js';
