c5359e37c3600efa58037b0c26526ac0b53ab7e7
Tomasz Andrzejak <andreiltd@users.noreply.github.com>
2025-06-06 09:11:02 +0200

    feat(p3-shim): implement wasi preview3 random API (#734)

--- packages/preview3-shim/lib/nodejs/index.js
+++ packages/preview3-shim/lib/nodejs/index.js
@@ -2,3 +2,4 @@ export * as clocks from './clocks.js';
 export * as filesystem from './filesystem.js';
 export * as stream from './stream.js';
 export * as future from './future.js';
+export * as random from './random.js';
