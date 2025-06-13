6c9095a0fb6aa497cfeb6d07fbea337840b532de
Victor Adossi <vadossi@cosmonic.com>
2025-06-13 16:37:19 +0900

    fix: reference to http-server virtualenv
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- crates/jco/tests/virtualenvs/http.mjs
+++ crates/jco/tests/virtualenvs/http.mjs
@@ -1,21 +1,23 @@
-import { _setEnv } from "@bytecodealliance/preview2-shim/cli";
-import { _setPreopens } from "@bytecodealliance/preview2-shim/filesystem";
 import { mkdtemp } from 'node:fs/promises';
 import { Worker } from 'node:worker_threads';
-import { fileURLToPath } from 'node:url';
+import { fileURLToPath, URL } from 'node:url';
+
+import { _setEnv } from '@bytecodealliance/preview2-shim/cli';
+import { _setPreopens } from '@bytecodealliance/preview2-shim/filesystem';
 import { _forbiddenHeaders } from '@bytecodealliance/preview2-shim/http';
 
 export const testDir = await mkdtemp('./tests/output/http');
 
-_setPreopens({ "/": testDir });
+_setPreopens({ '/': testDir });
 _forbiddenHeaders.add('custom-forbidden-header');
 
-const worker = new Worker(fileURLToPath(import.meta.url).slice(0, -7) + 'http-server.js');
-const PORT = await new Promise(resolve => worker.on('message', resolve));
+const worker = new Worker(
+    fileURLToPath(new URL('../http-server.mjs', import.meta.url))
+);
+const PORT = await new Promise((resolve) => worker.on('message', resolve));
 
 _setEnv({
-  'HTTP_SERVER': `localhost:${PORT}`
+    HTTP_SERVER: `localhost:${PORT}`,
 });
 
-if (worker.unref)
-  worker.unref();
+if (worker.unref) worker.unref();
