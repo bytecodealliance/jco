1ba32f05b3798a3b5eecbc31497532399da23e94
Victor Adossi <123968127+vados-cosmonic@users.noreply.github.com>
2025-07-19 21:01:52 +0900

    feat(ops): add 'docs' as valid scope
    Signed-off-by: Victor Adossi <123968127+vados-cosmonic@users.noreply.github.com>

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -29,6 +29,7 @@ const config = {
                 'deps',
                 'ci',
                 'ops',
+                'docs',
                 // Projects
                 'jco',
                 'bindgen',
