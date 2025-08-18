e356d26fea83207021774324845185652c531cfe
vados-cosmonic <123968127+vados-cosmonic@users.noreply.github.com>
2025-08-18 01:11:57 +0000

    release: jco v1.13.3
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- packages/jco/src/jco.js
+++ packages/jco/src/jco.js
@@ -25,7 +25,7 @@ program
     )
     .usage('<command> [options]')
     .enablePositionalOptions()
-    .version('1.13.2');
+    .version('1.13.3');
 
 function myParseInt(value) {
     return parseInt(value, 10);
