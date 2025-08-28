3fd245d4ee88ddd3a54f8ba8b4f0bdc84313c982
Victor Adossi <vadossi@cosmonic.com>
2025-08-28 22:42:52 +0900

    chore(jco): remove prettier in favor of eslint w/ fix
    This commit removes prettier in favor of eslint w/ `--fix`.
    While ideally we'd move to the oxc toolchain (when the formatter is
    available), for now we can address the mismatch between prettier and
    eslint by removing prettier.
    
    All this over an errant space in `function ()`.
    
    Resolves #860

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
-import prettier from 'eslint-plugin-prettier';
-import { default as prettierConfig } from './prettier.config.mjs';
 
 const config = defineConfig([
     globalIgnores(['test/output/**/*.js']),
     {
-        plugins: {
-            prettier,
-        },
         rules: {
+            indent: ['error', 4],
             'no-sparse-arrays': 0,
             'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
             'no-fallthrough': 0,
@@ -22,14 +18,6 @@ const config = defineConfig([
                     asyncArrow: 'always',
                 },
             ],
-
-            // NOTE: for some reason, prettier defaults are now 'function ()',
-            // so we use prettier fo format and eslint *after* to fix up issues.
-            //
-            // https://github.com/prettier/prettier/issues/1139
-            // https://github.com/prettier/prettier/issues/3847
-            // https://github.com/prettier/prettier/issues/3845
-            'prettier/prettier': ['off', prettierConfig],
         },
     },
 ]);
