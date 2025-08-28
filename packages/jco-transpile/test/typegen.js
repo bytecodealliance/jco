9d4a69633052211343df602758f09b8241ae4d5b
Victor Adossi <vadossi@cosmonic.com>
2025-08-28 23:41:03 +0900

    test(transpile): add test for JS reserved word generation

--- packages/jco-transpile/test/typegen.js
+++ packages/jco-transpile/test/typegen.js
@@ -2,7 +2,7 @@ import { Buffer } from 'node:buffer';
 
 import { suite, test, assert } from 'vitest';
 
-import { WIT_FIXTURE_DIR } from './helpers.js';
+import { WIT_FIXTURE_DIR, LOCAL_WIT_FIXTURE_DIR } from './helpers.js';
 
 import { generateGuestTypes, generateHostTypes } from '../src/typegen.js';
 
@@ -49,5 +49,68 @@ suite('Type Generation', () => {
                 "declare module 'test:flavorful/test' {"
             )
         );
+    });
+
+    // https://github.com/bytecodealliance/jco/issues/624
+    test('invalid js identifiers (guest import)', async () => {
+        const files = await generateGuestTypes(`${LOCAL_WIT_FIXTURE_DIR}/js-reserved-word.wit`, {
+            worldName: 'fixtures:js-reserved-word/imports',
+        });
+
+        assert.strictEqual(Object.keys(files).length, 2);
+        assert.strictEqual(
+            Object.keys(files)[1],
+            'interfaces/fixtures-js-reserved-word-example.d.ts'
+        );
+
+        const [ worldDeclaration, interfaceDeclaration ] = Object.values(files);
+        const worldDeclarationContent = Buffer.from(worldDeclaration);
+        const interfaceDeclarationContent = Buffer.from(interfaceDeclaration);
+
+        assert.ok(
+            worldDeclarationContent.includes(
+                "declare module 'fixtures:js-reserved-word/imports' {"
+            )
+        );
+        assert.ok(
+            interfaceDeclarationContent.includes(
+                "declare module 'fixtures:js-reserved-word/example' {"
+            )
+        );
+        assert(interfaceDeclarationContent.includes('export { _delete as delete };'));
+        assert(interfaceDeclarationContent.includes('function _delete'));
+
+    });
+
+    // https://github.com/bytecodealliance/jco/issues/624
+    test('invalid js identifiers (host import)', async () => {
+        const files = await generateHostTypes(`${LOCAL_WIT_FIXTURE_DIR}/js-reserved-word.wit`, {
+            worldName: 'fixtures:js-reserved-word/imports',
+        });
+
+        assert.strictEqual(Object.keys(files).length, 2);
+        assert.strictEqual(
+            Object.keys(files)[1],
+            'interfaces/fixtures-js-reserved-word-example.d.ts'
+        );
+
+        const [ worldDeclaration, interfaceDeclaration ] = Object.values(files);
+        const worldDeclarationContent = Buffer.from(worldDeclaration);
+        const interfaceDeclarationContent = Buffer.from(interfaceDeclaration);
+
+        assert(
+            worldDeclarationContent.includes(
+                "export type * as FixturesJsReservedWordExample"
+            ), 
+        );
+        assert(
+            interfaceDeclarationContent.includes(
+                "@module Interface fixtures:js-reserved-word/example"
+            ),
+        );
+
+        assert(interfaceDeclarationContent.includes('export { _delete as delete };'));
+        assert(interfaceDeclarationContent.includes('function _delete'));
+
     });
 });
