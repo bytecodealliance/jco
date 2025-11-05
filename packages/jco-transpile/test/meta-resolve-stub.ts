d178ef4b3f65b51d48598ef820e5e6e3b0cd065b
Victor Adossi <vadossi@cosmonic.com>
2025-11-05 18:22:26 +0900

    test(jco): refactor vite SSR hack for vitest

--- packages/jco-transpile/test/meta-resolve-stub.ts
+++
@@ -1,4 +0,0 @@
-// see: https://github.com/vitest-dev/vitest/issues/6953#issuecomment-2505310022
-import { vi } from 'vitest';
-import { createRequire } from 'node:module';
-vi.stubGlobal('globalCreateRequire', createRequire);
