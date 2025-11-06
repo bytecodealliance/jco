409c69b16102434733824e3a88f37424a5396cad
Victor Adossi <vadossi@cosmonic.com>
2025-11-06 11:52:50 +0900

    fix(std): working hacked response

--- packages/jco-std/src/wasi/0.2.6/http/types/response.ts
+++ packages/jco-std/src/wasi/0.2.6/http/types/response.ts
@@ -1,11 +1,13 @@
 import { OutgoingBody, ResponseOutparam, Fields, OutgoingResponse, FieldValue } from 'wasi:http/types@0.2.6';
 
+const ENCODER = new TextEncoder();
+
 /** Write a Web `Response` into a `wasi:http@0.2.x#outgoing-response` */
 export async function writeWebResponse(resp: Response, outgoingWasiResp: ResponseOutparam) {
-    const encoder = new TextEncoder();
+    // Build headers
     const fields: [string, FieldValue][] = [];
     for (const [k,v] of [...resp.headers.entries()]) {
-        fields.push([k.toString(), encoder.encode(v)]);
+        fields.push([k.toString(), ENCODER.encode(v)]);
     }
     const headers = Fields.fromList(fields);
     const outgoingResponse = new OutgoingResponse(headers);
@@ -22,35 +24,55 @@ export async function writeWebResponse(resp: Response, outgoingWasiResp: Respons
         if (resp.body === null) {
             throw new Error("unexpectedly missing resp.body");
         }
-        for await (const chunk of resp.body) {
+        const pollable = outputStream.subscribe();
+
+        // Create a reader for the body we'll be writing out
+        const reader = await resp.body.getReader();
+
+        while (true) {
+            const { value: chunk, done } = await reader.read();
+            if (done) { break; }
+
             if (chunk.length === 0) {
                 continue;
             }
-            let written = 0;
-            while (written < chunk.length) {
-                //let pollable = outputStream.subscribe();
-                const bytesAllowedRaw = outputStream.checkWrite();
-                if (!Number.isSafeInteger(bytesAllowedRaw)) {
-                    throw new Error("unexpectedly unsafe integer bytes allowed");
-                }
-                const bytesAllowed = Number(bytesAllowedRaw);
 
-                outputStream.write(
-                    new Uint8Array(chunk.buffer, written, bytesAllowed)
-                );
-                if (written + bytesAllowed > Number.MAX_VALUE) {
-                    throw new Error("integer overflow for written bytes");
+            let written = 0n;
+            while (written < chunk.length) {
+                // Wait until output stream is ready
+                if (!pollable.ready()) {
+                    pollable.block();
                 }
-                written += bytesAllowed;
+
+                // Find out how much we are allowed to write
+                const bytesAllowedRaw = outputStream.checkWrite();
+
+                // If we can't write as much as we want, we must
+                const remaining = BigInt(chunk.length) - written;
+                let pendingAmt;
+                if (remaining <= bytesAllowedRaw) {
+                    pendingAmt = chunk.length;
+                } else if (remaining > bytesAllowedRaw) {
+                    pendingAmt = bytesAllowedRaw;
+                }
+
+                // Write a view of the chunk in
+                const view = new Uint8Array(chunk, Number(written), pendingAmt);
+                outputStream.write(view);
+
+                written += BigInt(pendingAmt);
             }
         }
+
+        // Clean up pollable & stream
+        pollable[Symbol.dispose]();
         outputStream[Symbol.dispose]();
     }
 
     // Set the outgoing response body w/ no trailers
     OutgoingBody.finish(outgoingBody, undefined);
 
-    // Set the outparam
+    // Set the response outparam
     ResponseOutparam.set(outgoingWasiResp, {
         tag: 'ok',
         val: outgoingResponse,
