2faf2d331e1fb8b57d201181fb8ea32a60794878
Victor Adossi <vadossi@cosmonic.com>
2025-11-08 04:46:38 +0900

    refactor(examples): remove unneeded typescript plugin

--- examples/components/http-server-hono/rolldown.config.mjs
+++ examples/components/http-server-hono/rolldown.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from "rolldown";
-import typescript from "@rollup/plugin-typescript";
 
 export default defineConfig({
   input: "src/component.ts",
@@ -8,5 +7,4 @@ export default defineConfig({
     file: "dist/component.js",
     format: "esm",
   },
-    plugins: [typescript({ noEmitOnError: true })],
 });
