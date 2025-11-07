f5ea311aeb0b698973b71b999813ae6804ab7b0f
Victor Adossi <vadossi@cosmonic.com>
2025-11-07 19:06:29 +0900

    feat(std): remove star imports, add more docs

--- packages/jco-std/src/wasi/0.2.6/http/adapters/hono.ts
+++ packages/jco-std/src/wasi/0.2.6/http/adapters/hono.ts
@@ -11,6 +11,19 @@ import { buildEnvHelperFromWASI, type WasiEnvironmentHelper } from '../../../0.2
 import { readWASIRequest } from '../types/request.js';
 import { writeWebResponse } from '../types/response.js';
 
+type WASIEnvMiddleware = {
+    Variables: {
+        env: WasiEnvironmentHelper,
+    },
+};
+
+type WASIConfigMiddleware = {
+    Variables: {
+        config: WasiConfigHelper,
+    },
+};
+
+/** Analog to Hono's fire function, which sets up your Hono application to be served */
 export function fire<
     Env extends HonoEnv = HonoEnv,
     Schema extends HonoSchema = {}, // eslint-disable-line @typescript-eslint/no-empty-object-type
@@ -23,11 +36,35 @@ export function fire<
     realFireFn(app, opts);
 }
 
-type WASIEnvMiddleware = {
-    Variables: {
-        env: WasiEnvironmentHelper,
-    },
-};
+/**
+ * A Hono middleware that injects a helper that can retrieve
+ * environment variables from the platform on which the WebAssembly
+ * component is running.
+ *
+ * For example:
+ *
+ * ```
+ * import {
+ *     wasiEnvMiddleware
+ * } from '@bytecodealliance/jco-std/wasi/0.2.3/http/adapters/hono';
+ *
+ * const app = new Hono();
+ *
+ * app.use(wasiEnvMiddleware());
+ *
+ * app.post('/', async (c) => {
+ *     const env = c.get('env');
+ *     if (!env) { return c.json({ status: 'error', msg: 'missing env helper'}); }
+ *
+ *     const envValue = env.getAllObject()['SOME_KEY'];
+ *
+ *     return c.json({ status: 'success', data: envValue });
+ * });
+ * ```
+ *
+ * @see https://github.com/WebAssembly/wasi-cli
+ * @see https://github.com/WebAssembly/wasi-cli/blob/main/wit/environment.wit
+ */
 export const wasiEnvMiddleware = () => {
     return createMiddleware<WASIEnvMiddleware>(async (c, next) => {
         c.set('env', buildEnvHelperFromWASI(wasiEnv))
@@ -35,11 +72,35 @@ export const wasiEnvMiddleware = () => {
     });
 };
 
-type WASIConfigMiddleware = {
-    Variables: {
-        config: WasiConfigHelper,
-    },
-};
+/**
+ * A Hono middleware that injects a helper that can retrieve
+ * config variables from the platform on which the WebAssembly
+ * component is running.
+ *
+ * For example:
+ *
+ * ```
+ * import {
+ *     wasiConfigMiddleware
+ * } from '@bytecodealliance/jco-std/wasi/0.2.3/http/adapters/hono';
+ *
+ * const app = new Hono();
+ *
+ * app.use(wasiEnvMiddleware());
+ *
+ * app.post('/', async (c) => {
+ *     const config = c.get('config');
+ *     if (!config) { return c.json({ status: 'error', msg: 'missing env helper'}); }
+
+ *     const configValue = config.get('some-config-key');
+ *
+ *     return c.json({ status: 'success', data: configValue });
+ * });
+ * ```
+ *
+ * @see https://github.com/WebAssembly/wasi-config
+ * @see https://github.com/WebAssembly/wasi-config/blob/main/wit/store.wit
+ */
 export const wasiConfigMiddleware = () => {
     return createMiddleware<WASIConfigMiddleware>(async (c, next) => {
         c.set('config', buildConfigHelperFromWASI(wasiConfig))
