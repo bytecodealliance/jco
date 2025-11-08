aa7a76deb36bf8cde44385c9505b89d0439f6c28
Victor Adossi <vadossi@cosmonic.com>
2025-11-08 14:20:10 +0900

    fix(examples): try static node modules path

--- examples/components/http-server-hono/host-binding-wasi-config-store.mjs
+++ examples/components/http-server-hono/host-binding-wasi-config-store.mjs
@@ -4,7 +4,8 @@
  * It is used in the demo (see scripts/demo.mjs), and is mapped *into* the transpiled
  * component dynamically when it is served via `jco serve`.
  *
- * A module like this could also be used as an input during transpilation as a mapping argument.
+ * A module like this could also be used as an input during `jco transpile`transpilation 
+ * as a mapping (`--map`) argument.
  *
  * see: https://github.com/WebAssembly/wasi-config
  * see: https://github.com/WebAssembly/wasi-config/blob/main/wit/store.wit
