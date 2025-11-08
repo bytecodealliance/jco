127fcefb4f8a0efd023e35e081384ce33168d7cb
Victor Adossi <vadossi@cosmonic.com>
2025-11-08 15:05:46 +0900

    fix(ci): allow setup for examples

--- examples/components/http-server-hono/rolldown.config.mjs
+++ examples/components/http-server-hono/rolldown.config.mjs
@@ -1,22 +1,4 @@
-import { env } from "node:process";
-import { URL, fileURLToPath } from "node:url";
-import { join } from "node:path";
-
 import { defineConfig } from "rolldown";
-
-// For some reason, rolldown will not properly resolve node modules when
-// in GitHub CI.
-//
-// The code below creates a manual mapping of the jco-std imports (and modified alias)
-// for CI environments and is NOT needed in a regular project
-const WORKSPACE_JCO_STD = fileURLToPath(new URL("../../../node_modules/@bytecodealliance/jco-std", import.meta.url));
-
-const alias = env.CI ? {
-    '@bytecodealliance/jco-std/wasi/0.2.3/http/adapters/hono': join(WORKSPACE_JCO_STD, "dist/0.2.3/http/adapters/hono.js"),
-    '@bytecodealliance/jco-std/wasi/0.2.6/http/adapters/hono': join(WORKSPACE_JCO_STD, "dist/0.2.6/http/adapters/hono.js"),
-} : {};
-
-console.log("ALIAS?", alias);
 
 export default defineConfig({
     input: "src/component.ts",
@@ -25,5 +7,4 @@ export default defineConfig({
         file: "dist/component.js",
         format: "esm",
     },
-    resolve: { alias },
 });
