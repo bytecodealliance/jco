ae55110458232c97b2a1c4ee9f5884ebe74a9cd6
Victor Adossi <vadossi@cosmonic.com>
2025-11-08 14:20:10 +0900

    fix(examples): try static node modules path

--- examples/components/http-server-hono/rolldown.config.mjs
+++ examples/components/http-server-hono/rolldown.config.mjs
@@ -1,4 +1,8 @@
+import { URL, fileURLToPath } from "node:url";
+
 import { defineConfig } from "rolldown";
+
+const WORKSPACE_NODE_MODULES = fileURLToPath(new URL("../../../node_modules", import.meta.url));
 
 export default defineConfig({
     input: "src/component.ts",
@@ -11,6 +15,7 @@ export default defineConfig({
         modules: [
             "node_modules",
             "../../../node_modules",
+            WORKSPACE_NODE_MODULES,
         ]
     },
 });
