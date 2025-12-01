dc323f37c44a11d1585dadb88f7a351386206f84
Victor Adossi <vadossi@cosmonic.com>
2025-12-01 22:02:10 +0900

    fix(jco): async return writing for caller memory
    This commit fixes a bug where the memory for writing to was being
    retrieved too eagerly (@ trampoline fn binding time), and was not
    available properly when performing async `task.return`s.

--- packages/jco/test/p3/async.js
+++ packages/jco/test/p3/async.js
@@ -10,24 +10,16 @@ import { P3_COMPONENT_FIXTURES_DIR } from '../common.js';
 suite('Async (WASI P3)', () => {
     // see: https://github.com/bytecodealliance/jco/issues/1076
     test('incorrect task return params offloading', async () => {
-        const componentPath = join(
-            P3_COMPONENT_FIXTURES_DIR,
-            'async-flat-param-adder.wasm'
-        );
-
+        const name = 'async-flat-param-adder';
         const { instance, cleanup } = await setupAsyncTest({
             component: {
-                name: 'async-flat-param-adder',
-                path: componentPath,
+                name,
+                path: join(
+                    P3_COMPONENT_FIXTURES_DIR,
+                    `${name}.wasm`,
+                ),
                 imports: new WASIShim().getImportObject(),
             },
-            jco: {
-                transpile: {
-                    extraArgs: {
-                        minify: false,
-                    }
-                }
-            }
         });
 
         assert(instance.test3);
@@ -35,6 +27,28 @@ suite('Async (WASI P3)', () => {
 
         const result = await instance.test3.asyncTest4(2,2);
         assert.strictEqual(result, 4);
+
+        await cleanup();
+    });
+
+    // https://bytecodealliance.zulipchat.com/#narrow/channel/206238-general/topic/Should.20StringLift.20be.20emitted.20for.20async.20return.20values.3F/with/561133720
+    test('simple string return', async () => {
+        const name = 'async-simple-string-return';
+        const { instance, cleanup } = await setupAsyncTest({
+            component: {
+                name,
+                path: join(
+                    P3_COMPONENT_FIXTURES_DIR,
+                    `${name}.wasm`,
+                ),
+                imports: new WASIShim().getImportObject(),
+            },
+        });
+
+        assert.typeOf(instance.asyncGetLiteral, 'function');
+
+        const result = await instance.asyncGetLiteral();
+        assert.strictEqual(result, "literal");
 
         await cleanup();
     });
