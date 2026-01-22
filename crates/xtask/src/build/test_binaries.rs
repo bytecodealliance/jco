7cbaf45c2232e00c4e7115b6699904dfec7c0817
Victor Adossi <vadossi@cosmonic.com>
2026-01-22 18:18:04 +0900

    fix(ops): clippy lint

--- crates/xtask/src/build/test_binaries.rs
+++ crates/xtask/src/build/test_binaries.rs
@@ -3,14 +3,11 @@ use std::sync::LazyLock;
 
 use anyhow::{Result, bail};
 
-static WORKSPACE_DIR: LazyLock<PathBuf> = LazyLock::new(|| {
-    // NOTE this goes to the xtask dir
-    let xtask_manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    xtask_manifest_dir.join("../../")
-});
+static WORKSPACE_DIR: LazyLock<PathBuf> =
+    LazyLock::new(|| PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../../"));
 
 /// Build the test binaries
 pub(crate) fn run() -> Result<()> {
-    
-    Ok(())
+    let _ = WORKSPACE_DIR;
+    bail!("not implemented")
 }
