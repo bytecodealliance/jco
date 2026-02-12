9ca0f4f4d6b0618d1405f10b37c1e873d16234d8
Victor Adossi <vadossi@cosmonic.com>
2026-02-12 21:27:31 +0900

    chore(transpile): swap eslint for oxlint

--- packages/jco-transpile/flavorful.d.ts
+++ packages/jco-transpile/flavorful.d.ts
@@ -1,4 +1,3 @@
-/// <reference path="./interfaces/test-flavorful-test.d.ts" />
 declare module 'test:flavorful/flavorful' {
   export type * as TestFlavorfulTest from 'test:flavorful/test'; // import test:flavorful/test
   export * as test from 'test:flavorful/test'; // export test:flavorful/test
