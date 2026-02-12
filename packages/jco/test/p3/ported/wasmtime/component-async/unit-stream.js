fbd6ad3ccddf3b4ee72eb85bb82caa322d9ca2d7
Victor Adossi <vadossi@cosmonic.com>
2026-02-12 23:09:57 +0900

    test(jco): add tests for p3 streams

--- packages/jco/test/p3/ported/wasmtime/component-async/unit-stream.js
+++ packages/jco/test/p3/ported/wasmtime/component-async/unit-stream.js
@@ -7,41 +7,17 @@ import { buildAndTranspile, composeCallerCallee, COMPONENT_FIXTURES_DIR } from "
 // These tests are ported from upstream wasmtime's component-async-tests
 //
 // In the upstream wasmtime repo, see:
-// wasmtime/crates/misc/component-async-tests/tests/scenario/error_context.rs
+// wasmtime/crates/misc/component-async-tests/tests/scenario/unit-stream.rs
 //
-suite('error-context scenario', () => {
-    test('single-component', async () => {
-        const componentPath = join(
-            COMPONENT_FIXTURES_DIR,
-            'p3/error-context/async-error-context.wasm'
-        );
-
-        let cleanup;
-        try {
-            const res = await buildAndTranspile({
-                componentPath,
-                // transpile: {
-                //     extraArgs: {
-                //         minify: false,
-                //     },
-                // },
-            });
-            const instance = res.instance;
-            cleanup = res.cleanup;
-            await instance['local:local/run'].run();
-        } finally {
-            if (cleanup) { await cleanup(); }
-        }
-    });
-
+suite('unit streams scenario', () => {
     test('caller & callee', async () => {
         const callerPath = join(
             COMPONENT_FIXTURES_DIR,
-            "p3/error-context/async-error-context-caller.wasm"
+            "p3/streams/async-unit-stream-caller.wasm"
         );
         const calleePath = join(
             COMPONENT_FIXTURES_DIR,
-            "p3/error-context/async-error-context-callee.wasm"
+            "p3/streams/async-unit-stream-callee.wasm"
         );
         const componentPath = await composeCallerCallee({
             callerPath,
@@ -55,12 +31,11 @@ suite('error-context scenario', () => {
                 // transpile: {
                 //     extraArgs: {
                 //         minify: false,
-                //     },
-                // },
+                //     }
+                // }
             });
-            cleanup = res.cleanup;
             const instance = res.instance;
-
+            cleanup = res.cleanup;
             await instance['local:local/run'].run();
         } finally {
             if (cleanup) { await cleanup(); }
---
+++ packages/jco/test/p3/stream.js
@@ -0,0 +1,61 @@
+import { join } from 'node:path';
+
+import { suite, test, assert } from 'vitest';
+
+import { setupAsyncTest } from '../helpers.js';
+import { AsyncFunction, LOCAL_TEST_COMPONENTS_DIR } from '../common.js';
+
+suite('Stream (WASI P3)', () => {
+    test('stream<u32> (tx)', async () => {
+        const name = 'stream-tx';
+        const { esModule, cleanup } = await setupAsyncTest({
+            asyncMode: 'jspi',
+            component: {
+                name,
+                path: join(LOCAL_TEST_COMPONENTS_DIR, `${name}.wasm`),
+                skipInstantiation: true,
+            },
+            jco: {
+                transpile: {
+                    extraArgs: {
+                        minify: false,
+                        asyncExports: [
+                            'jco:test-components/get-stream-async#get-stream-u32',
+                            'jco:test-components/get-stream-async#get-stream-s32',
+                        ],
+                    },
+                },
+            },
+        });
+
+        const { WASIShim } = await import(
+            '@bytecodealliance/preview2-shim/instantiation'
+        );
+        const instance = await esModule.instantiate(
+            undefined,
+            new WASIShim().getImportObject()
+        );
+
+        let vals;
+        let stream;
+
+        assert.instanceOf(instance['jco:test-components/get-stream-async'].getStreamU32, AsyncFunction);
+        assert.instanceOf(instance['jco:test-components/get-stream-async'].getStreamS32, AsyncFunction);
+
+        vals = [11,22,33];
+        stream = await instance['jco:test-components/get-stream-async'].getStreamU32(vals);
+        assert.equal(vals[0], await stream.next(), "first u32 read is incorrect");
+        assert.equal(vals[1], await stream.next(), "second u32 read is incorrect");
+        assert.equal(vals[2], await stream.next(), "third u32 read is incorrect");
+        // TODO(tests): we should check that reading with no values remaining blocks?
+        // TODO(tests): we should check that reading when writer is closed throws error?
+
+        vals = [-11,-22,-33];
+        stream = await instance['jco:test-components/get-stream-async'].getStreamS32(vals);
+        assert.equal(vals[0], await stream.next());
+        assert.equal(vals[1], await stream.next());
+        assert.equal(vals[2], await stream.next());
+
+        await cleanup();
+    });
+});
