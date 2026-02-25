05b922720bf8a57500d3bfc2556ab8fa3cd9b958
Victor Adossi <vadossi@cosmonic.com>
2026-02-26 02:00:14 +0900

    chore(examples): address code review comments

---
+++ examples/components/node-compat/rolldown.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from "rolldown";
+import { defineEnv as defineUnenv } from "unenv";
+
+// NOTE: the line below creates an unenv environment,
+// there are various variables you can control here (including
+// supplying your own implementations for NodeJS builtins).
+//
+// see: https://github.com/unjs/unenv for more information
+//
+const unenv = defineUnenv();
+
+export default defineConfig({
+    input: "src/component.js",
+    external: /wasi:.*/,
+    output: {
+        file: "dist/guest/component.js",
+        format: "esm",
+    },
+    resolve: {
+        alias: {
+            ...unenv.env.alias,
+        },
+    },
+});
