810e93bbe27331829c5acbe5d549325f6962e316
Victor Adossi <vadossi@cosmonic.com>
2026-03-17 00:59:54 +0900

    test(jco): add stream<string> test, refactor & uncomment existing

--- packages/jco/test/p3/stream.js
+++ packages/jco/test/p3/stream.js
@@ -1,14 +1,17 @@
 import { join } from "node:path";
 
-import { suite, test, assert, expect, vi } from "vitest";
+import { suite, test, assert, expect, vi, beforeAll, beforeEach, afterAll } from "vitest";
 
 import { setupAsyncTest } from "../helpers.js";
 import { AsyncFunction, LOCAL_TEST_COMPONENTS_DIR } from "../common.js";
+import { WASIShim } from "@bytecodealliance/preview2-shim/instantiation";
 
-suite("Stream (WASI P3)", () => {
-    test("stream<_> (tx)", async () => {
+suite("stream<T> lifts", () => {
+    let esModule, cleanup, instance;
+
+    beforeAll(async () => {
         const name = "stream-tx";
-        const { esModule, cleanup } = await setupAsyncTest({
+        const setupRes = await setupAsyncTest({
             asyncMode: "jspi",
             component: {
                 name,
@@ -19,70 +22,82 @@ suite("Stream (WASI P3)", () => {
                 transpile: {
                     extraArgs: {
                         minify: false,
-                        asyncExports: [
-                            "jco:test-components/get-stream-async#get-stream-u32",
-                            "jco:test-components/get-stream-async#get-stream-s32",
-                            "jco:test-components/get-stream-async#get-stream-bool",
-                            "jco:test-components/get-stream-async#get-stream-u8",
-                            "jco:test-components/get-stream-async#get-stream-s8",
-                            "jco:test-components/get-stream-async#get-stream-u16",
-                            "jco:test-components/get-stream-async#get-stream-s16",
-                            "jco:test-components/get-stream-async#get-stream-u64",
-                            "jco:test-components/get-stream-async#get-stream-s64",
-                            "jco:test-components/get-stream-async#get-stream-f32",
-                            "jco:test-components/get-stream-async#get-stream-f64",
-                        ],
                     },
                 },
             },
         });
 
-        const { WASIShim } = await import("@bytecodealliance/preview2-shim/instantiation");
-        const instance = await esModule.instantiate(undefined, new WASIShim().getImportObject());
+        esModule = setupRes.esModule;
+        cleanup = setupRes.cleanup;
+    });
 
-        let vals;
-        let stream;
+    afterAll(async () => {
+        await cleanup();
+    });
 
+    beforeEach(async () => {
+        instance = await esModule.instantiate(undefined, new WASIShim().getImportObject());
+    });
+
+    test("u32/s32", async () => {
         assert.instanceOf(instance["jco:test-components/get-stream-async"].getStreamU32, AsyncFunction);
         assert.instanceOf(instance["jco:test-components/get-stream-async"].getStreamS32, AsyncFunction);
-        assert.instanceOf(instance["jco:test-components/get-stream-async"].getStreamBool, AsyncFunction);
-        assert.instanceOf(instance["jco:test-components/get-stream-async"].getStreamU8, AsyncFunction);
-        assert.instanceOf(instance["jco:test-components/get-stream-async"].getStreamS8, AsyncFunction);
-        assert.instanceOf(instance["jco:test-components/get-stream-async"].getStreamU16, AsyncFunction);
-        assert.instanceOf(instance["jco:test-components/get-stream-async"].getStreamS16, AsyncFunction);
-        assert.instanceOf(instance["jco:test-components/get-stream-async"].getStreamU64, AsyncFunction);
-        assert.instanceOf(instance["jco:test-components/get-stream-async"].getStreamS64, AsyncFunction);
-        assert.instanceOf(instance["jco:test-components/get-stream-async"].getStreamF64, AsyncFunction);
-        assert.instanceOf(instance["jco:test-components/get-stream-async"].getStreamF32, AsyncFunction);
 
-        vals = [11, 22, 33];
-        stream = await instance["jco:test-components/get-stream-async"].getStreamU32(vals);
-        await checkStreamValues(stream, vals, "u32");
-
-        vals = [true, false];
-        stream = await instance["jco:test-components/get-stream-async"].getStreamBool(vals);
+        let vals = [11, 22, 33];
+        let stream = await instance["jco:test-components/get-stream-async"].getStreamU32(vals);
         await checkStreamValues(stream, vals, "u32");
 
         vals = [-11, -22, -33];
         stream = await instance["jco:test-components/get-stream-async"].getStreamS32(vals);
-        await checkStreamValues(stream, vals, "u32");
+        await checkStreamValues(stream, vals, "s32");
+    });
 
-        // TODO(fix): attempting to stream non-null/numeric values traps, and the component cannot currently recover
-        // TODO(fix): this is *not* a inter-component non-null/numeric value send!
+    test("bool", async () => {
+        assert.instanceOf(instance["jco:test-components/get-stream-async"].getStreamBool, AsyncFunction);
+        const vals = [true, false];
+        const stream = await instance["jco:test-components/get-stream-async"].getStreamBool(vals);
+        await checkStreamValues(stream, vals, "bool");
+    });
+
+    test("u16/s16", async () => {
+        assert.instanceOf(instance["jco:test-components/get-stream-async"].getStreamU16, AsyncFunction);
+        assert.instanceOf(instance["jco:test-components/get-stream-async"].getStreamS16, AsyncFunction);
+
+        let vals = [0, 100, 65535];
+        let stream = await instance["jco:test-components/get-stream-async"].getStreamU16(vals);
+        await checkStreamValues(stream, vals, "u16");
+        // TODO(fix): under/overflowing values hang
+
+        vals = [-32_768, 0, 32_767];
+        stream = await instance["jco:test-components/get-stream-async"].getStreamS16(vals);
+        await checkStreamValues(stream, vals, "u16");
+        // TODO(fix): under/overflowing values hang
+    });
+
+    test("u64/s64", async () => {
+        assert.instanceOf(instance["jco:test-components/get-stream-async"].getStreamU64, AsyncFunction);
+        assert.instanceOf(instance["jco:test-components/get-stream-async"].getStreamS64, AsyncFunction);
+
+        let vals = [0n, 100n, 65535n];
+        let stream = await instance["jco:test-components/get-stream-async"].getStreamU64(vals);
+        await checkStreamValues(stream, vals, "u64");
+        // TODO(fix): under/overflowing values hang
+
+        vals = [-32_768n, 0n, 32_767n];
+        stream = await instance["jco:test-components/get-stream-async"].getStreamS64(vals);
+        await checkStreamValues(stream, vals, "s64");
+        // TODO(fix): under/overflowing values hang
+    });
+
+    // // TODO(fix): stream gets stuck waiting, u8/s8 require special case
+    test.skip("u8/s8", async () => {
+        assert.instanceOf(instance["jco:test-components/get-stream-async"].getStreamU8, AsyncFunction);
+        assert.instanceOf(instance["jco:test-components/get-stream-async"].getStreamS8, AsyncFunction);
+
+        // let vals = [0, 1, 255];
+        // let stream = await instance["jco:test-components/get-stream-async"].getStreamU8(vals);
+        // await checkStreamValues(stream, vals, "u8");
         //
-        // vals = [true];
-        // stream = await instance["jco:test-components/get-stream-async"].getStreamBool(vals);
-        // await expect(() => stream.next()).rejects.toThrowError(/cannot stream non-numeric types within the same component/);
-
-        // // TODO(fix): stream produces invalid values here (undefined),
-        // // likely due to, streams of u8/s8s needing to be a special case
-        // vals = [0, 1, 255];
-        // stream = await instance["jco:test-components/get-stream-async"].getStreamU8(vals);
-        // assert.equal(vals[0], await stream.next());
-        // assert.equal(vals[1], await stream.next());
-        // assert.equal(vals[2], await stream.next());
-
-        // // TODO(fix): stream gets stuck waiting, u8/s8 require special case
         // vals = [-11, -22, -33, -128, 127, 128];
         // stream = await instance["jco:test-components/get-stream-async"].getStreamS8(vals);
         // assert.equal(vals[0], await stream.next());
@@ -90,52 +105,37 @@ suite("Stream (WASI P3)", () => {
         // assert.equal(vals[2], await stream.next());
         // assert.equal(vals[3], await stream.next());
         // assert.equal(vals[4], await stream.next());
+    });
 
-        // TODO(fix): we're stuck waiting for the previous task to complete?? It's waiting for waitable set?
-        // read end needs to be dropped at some point, and it's not!
-        vals = [0, 100, 65535];
-        stream = await instance["jco:test-components/get-stream-async"].getStreamU16(vals);
-        await checkStreamValues(stream, vals, "u32");
-        // TODO(fix): under/overflowing values hang
+    test("f32/f64", async () => {
+        assert.instanceOf(instance["jco:test-components/get-stream-async"].getStreamF64, AsyncFunction);
+        assert.instanceOf(instance["jco:test-components/get-stream-async"].getStreamF32, AsyncFunction);
 
-        vals = [-32_768, 0, 32_767];
-        stream = await instance["jco:test-components/get-stream-async"].getStreamS16(vals);
-        await checkStreamValues(stream, vals, "u32");
-        // TODO(fix): under/overflowing values hang
+        let vals = [-300.01235, -1.5, -0.0, 0.0, 1.5, 300.01235];
+        let stream = await instance["jco:test-components/get-stream-async"].getStreamF32(vals);
+        assert.closeTo(vals[0], await stream.next(), 0.00001);
+        assert.closeTo(vals[1], await stream.next(), 0.00001);
+        assert.closeTo(vals[2], await stream.next(), 0.00001);
+        assert.closeTo(vals[3], await stream.next(), 0.00001);
+        assert.closeTo(vals[4], await stream.next(), 0.00001);
+        assert.closeTo(vals[5], await stream.next(), 0.00001);
 
-        vals = [0n, 100n, 65535n];
-        stream = await instance["jco:test-components/get-stream-async"].getStreamU64(vals);
-        await checkStreamValues(stream, vals, "u32");
-        // TODO(fix): under/overflowing values hang
+        vals = [-300.01235, -1.5, -0.0, 0.0, 1.5, -300.01235];
+        stream = await instance["jco:test-components/get-stream-async"].getStreamF64(vals);
+        assert.closeTo(vals[0], await stream.next(), 0.00001);
+        assert.closeTo(vals[1], await stream.next(), 0.00001);
+        assert.closeTo(vals[2], await stream.next(), 0.00001);
+        assert.closeTo(vals[3], await stream.next(), 0.00001);
+        assert.closeTo(vals[4], await stream.next(), 0.00001);
+        assert.closeTo(vals[5], await stream.next(), 0.00001);
+    });
 
-        vals = [-32_768n, 0n, 32_767n];
-        stream = await instance["jco:test-components/get-stream-async"].getStreamS64(vals);
-        await checkStreamValues(stream, vals, "u32");
-        // TODO(fix): under/overflowing values hang
+    test("string", async () => {
+        assert.instanceOf(instance["jco:test-components/get-stream-async"].getStreamString, AsyncFunction);
 
-        // // TODO(fix): add better edge cases
-        // // TODO(fix): valid values hang
-        // vals = [-300.01235, -1.5, -0.0, 0.0, 1.5, 300.01235 ];
-        // stream = await instance["jco:test-components/get-stream-async"].getStreamF32(vals);
-        // assert.closeTo(vals[0], await stream.next(), 0.00001);
-        // assert.closeTo(vals[1], await stream.next(), 0.00001);
-        // assert.closeTo(vals[2], await stream.next(), 0.00001);
-        // assert.closeTo(vals[3], await stream.next(), 0.00001);
-        // assert.closeTo(vals[4], await stream.next(), 0.00001);
-        // assert.closeTo(vals[5], await stream.next(), 0.00001);
-
-        // // TODO(fix): add better edge cases
-        // // TODO(fix): valid values hang
-        // vals = [-300.01235, -1.5, -0.0, 0.0, 1.5, -300.01235 ];
-        // stream = await instance["jco:test-components/get-stream-async"].getStreamF64(vals);
-        // assert.closeTo(vals[0], await stream.next(), 0.00001);
-        // assert.closeTo(vals[1], await stream.next(), 0.00001);
-        // assert.closeTo(vals[2], await stream.next(), 0.00001);
-        // assert.closeTo(vals[3], await stream.next(), 0.00001);
-        // assert.closeTo(vals[4], await stream.next(), 0.00001);
-        // assert.closeTo(vals[5], await stream.next(), 0.00001);
-
-        await cleanup();
+        let vals = ["hello", "world", "!"];
+        let stream = await instance["jco:test-components/get-stream-async"].getStreamString(vals);
+        await checkStreamValues(stream, vals, "string");
     });
 });
 
