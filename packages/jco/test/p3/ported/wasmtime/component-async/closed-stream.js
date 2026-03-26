a6430706d17669fbae9c4c499beec40cd8af98d0
Victor Adossi <vadossi@cosmonic.com>
2026-03-27 01:06:51 +0900

    test(jco): add test for sync closed stream

---
+++ packages/jco/test/p3/ported/wasmtime/component-async/closed-stream.js
@@ -0,0 +1,39 @@
+import { join } from "node:path";
+
+import { assert, suite, test, beforeAll, afterAll, beforeEach, afterEach } from "vitest";
+
+import { buildAndTranspile, COMPONENT_FIXTURES_DIR } from "./common.js";
+import { WASIShim } from "@bytecodealliance/preview2-shim/instantiation";
+
+// These tests are ported from upstream wasmtime's component-async-tests
+//
+// In the upstream wasmtime repo, see:
+// wasmtime/crates/misc/component-async-tests/tests/scenario/streams.rs
+//
+suite("closed stream scenario", () => {
+    test("sync instantly dropped stream", async () => {
+        const componentPath = join(COMPONENT_FIXTURES_DIR, "p3/streams/async-closed-stream.wasm");
+        let cleanup;
+        try {
+            const res = await buildAndTranspile({
+                componentPath,
+                transpile: {
+                    extraArgs: {
+                        minify: false,
+                    }
+                }
+            });
+            const instance = res.instance;
+            cleanup = res.cleanup;
+
+            const stream = instance["local:local/closed-stream"].get();
+            const v = await stream.next();
+            assert.strictEqual(v, undefined);
+
+        } finally {
+            if (cleanup) {
+                await cleanup();
+            }
+        }
+    });
+});
