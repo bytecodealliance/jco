51f1902c212784797840f0d4982c40c953632ba1
Victor Adossi <vadossi@cosmonic.com>
2026-04-17 11:10:39 +0900

    fix(jco): version number

--- packages/jco/src/jco.js
+++ packages/jco/src/jco.js
@@ -25,7 +25,7 @@ program
     )
     .usage("<command> [options]")
     .enablePositionalOptions()
-    .version("1.16.1");
+    .version("1.17.7");
 
 function myParseInt(value) {
     return parseInt(value, 10);
