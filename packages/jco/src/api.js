d1bdce8ac91aec7f957aeca1c5ead15ee9ec767b
Spencer Young <spencerwyoung@outlook.com>
2026-05-18 20:31:36 -0700

    feat(jco): explicit types for jco API exports

--- packages/jco/src/api.js
+++ packages/jco/src/api.js
@@ -1,3 +1,4 @@
+// @ts-check
 export { optimizeComponent as opt } from "./cmd/opt.js";
 export { transpileComponent as transpile, typesComponent as types } from "./cmd/transpile.js";
 import { $init, tools } from "../obj/wasm-tools.js";
@@ -12,63 +13,57 @@ const {
 } = tools;
 
 /**
- * @param {Parameters<import('../obj/wasm-tools.js').print>[0]} binary
- * @return {Promise<ReturnType<import('../obj/wasm-tools.js').print>>}
+ * @param {Uint8Array} binary
  */
 export async function print(binary) {
     await $init;
     return printFn(binary);
 }
 /**
- * @param {Parameters<import('../obj/wasm-tools.js').parse>[0]} wat
- * @return {Promise<ReturnType<import('../obj/wasm-tools.js').parse>>}
+ * @param {string} wat
  */
 export async function parse(wat) {
     await $init;
     return parseFn(wat);
 }
 /**
- * @param {Parameters<import('../obj/wasm-tools.js').componentWit>[0]} binary
- * @return {Promise<ReturnType<import('../obj/wasm-tools.js').componentWit>>}
+ * @param {Uint8Array} binary
  */
 export async function componentWit(binary) {
     await $init;
     return componentWitFn(binary);
 }
 /**
- * @param {Parameters<import('../obj/wasm-tools.js').componentNew>[0]} binary
- * @param {Parameters<import('../obj/wasm-tools.js').componentNew>[1]} adapters
- * @return {Promise<ReturnType<import('../obj/wasm-tools.js').componentNew>>}
+ * @param {Uint8Array} binary
+ * @param {Array<[string, Uint8Array]>} [adapters]
  */
 export async function componentNew(binary, adapters) {
     await $init;
     return componentNewFn(binary, adapters);
 }
 /**
- * @param {Parameters<import('../obj/wasm-tools.js').componentEmbed>[0]} embedOpts
- * @return {Promise<ReturnType<import('../obj/wasm-tools.js').componentEmbed>>}
+ * @param {tools.EmbedOpts} embedOpts
  */
 export async function componentEmbed(embedOpts) {
     await $init;
     return componentEmbedFn(embedOpts);
 }
 /**
- * @param {Parameters<import('../obj/wasm-tools.js').metadataAdd>[0]} binary
- * @param {Parameters<import('../obj/wasm-tools.js').metadataAdd>[1]} metadata
- * @return {Promise<ReturnType<import('../obj/wasm-tools.js').metadataAdd>>}
+ * @param {Uint8Array} binary
+ * @param {tools.ProducersFields} metadata
  */
 export async function metadataAdd(binary, metadata) {
     await $init;
     return metadataAddFn(binary, metadata);
 }
 /**
- * @param {Parameters<import('../obj/wasm-tools.js').metadataShow>[0]} binary
- * @return {Promise<ReturnType<import('../obj/wasm-tools.js').metadataShow>>}
+ * @param {Uint8Array} binary
  */
 export async function metadataShow(binary) {
     await $init;
     return metadataShowFn(binary);
 }
+
 export function preview1AdapterCommandPath() {
     return new URL("../lib/wasi_snapshot_preview1.command.wasm", import.meta.url);
 }
