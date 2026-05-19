6817a92b8c9ab9ec401d2af5aa5b14f1dfb787b8
Spencer Young <spencerwyoung@outlook.com>
2026-05-18 20:31:36 -0700

    feat(jco): explicit types for jco API exports

--- packages/jco/types/api.d.ts
+++ packages/jco/types/api.d.ts
@@ -1,42 +1,36 @@
 /**
- * @param {Parameters<import('../obj/wasm-tools.js').print>[0]} binary
- * @return {Promise<ReturnType<import('../obj/wasm-tools.js').print>>}
+ * @param {Uint8Array} binary
  */
-export function print(binary: Parameters<import("../obj/wasm-tools.js").print>[0]): Promise<ReturnType<import("../obj/wasm-tools.js").print>>;
+export function print(binary: Uint8Array): Promise<string>;
 /**
- * @param {Parameters<import('../obj/wasm-tools.js').parse>[0]} wat
- * @return {Promise<ReturnType<import('../obj/wasm-tools.js').parse>>}
+ * @param {string} wat
  */
-export function parse(wat: Parameters<import("../obj/wasm-tools.js").parse>[0]): Promise<ReturnType<import("../obj/wasm-tools.js").parse>>;
+export function parse(wat: string): Promise<Uint8Array<ArrayBufferLike>>;
 /**
- * @param {Parameters<import('../obj/wasm-tools.js').componentWit>[0]} binary
- * @return {Promise<ReturnType<import('../obj/wasm-tools.js').componentWit>>}
+ * @param {Uint8Array} binary
  */
-export function componentWit(binary: Parameters<import("../obj/wasm-tools.js").componentWit>[0]): Promise<ReturnType<import("../obj/wasm-tools.js").componentWit>>;
+export function componentWit(binary: Uint8Array): Promise<string>;
 /**
- * @param {Parameters<import('../obj/wasm-tools.js').componentNew>[0]} binary
- * @param {Parameters<import('../obj/wasm-tools.js').componentNew>[1]} adapters
- * @return {Promise<ReturnType<import('../obj/wasm-tools.js').componentNew>>}
+ * @param {Uint8Array} binary
+ * @param {Array<[string, Uint8Array]>} [adapters]
  */
-export function componentNew(binary: Parameters<import("../obj/wasm-tools.js").componentNew>[0], adapters: Parameters<import("../obj/wasm-tools.js").componentNew>[1]): Promise<ReturnType<import("../obj/wasm-tools.js").componentNew>>;
+export function componentNew(binary: Uint8Array, adapters?: Array<[string, Uint8Array]>): Promise<Uint8Array<ArrayBufferLike>>;
 /**
- * @param {Parameters<import('../obj/wasm-tools.js').componentEmbed>[0]} embedOpts
- * @return {Promise<ReturnType<import('../obj/wasm-tools.js').componentEmbed>>}
+ * @param {tools.EmbedOpts} embedOpts
  */
-export function componentEmbed(embedOpts: Parameters<import("../obj/wasm-tools.js").componentEmbed>[0]): Promise<ReturnType<import("../obj/wasm-tools.js").componentEmbed>>;
+export function componentEmbed(embedOpts: tools.EmbedOpts): Promise<Uint8Array<ArrayBufferLike>>;
 /**
- * @param {Parameters<import('../obj/wasm-tools.js').metadataAdd>[0]} binary
- * @param {Parameters<import('../obj/wasm-tools.js').metadataAdd>[1]} metadata
- * @return {Promise<ReturnType<import('../obj/wasm-tools.js').metadataAdd>>}
+ * @param {Uint8Array} binary
+ * @param {tools.ProducersFields} metadata
  */
-export function metadataAdd(binary: Parameters<import("../obj/wasm-tools.js").metadataAdd>[0], metadata: Parameters<import("../obj/wasm-tools.js").metadataAdd>[1]): Promise<ReturnType<import("../obj/wasm-tools.js").metadataAdd>>;
+export function metadataAdd(binary: Uint8Array, metadata: tools.ProducersFields): Promise<Uint8Array<ArrayBufferLike>>;
 /**
- * @param {Parameters<import('../obj/wasm-tools.js').metadataShow>[0]} binary
- * @return {Promise<ReturnType<import('../obj/wasm-tools.js').metadataShow>>}
+ * @param {Uint8Array} binary
  */
-export function metadataShow(binary: Parameters<import("../obj/wasm-tools.js").metadataShow>[0]): Promise<ReturnType<import("../obj/wasm-tools.js").metadataShow>>;
+export function metadataShow(binary: Uint8Array): Promise<tools.ModuleMetadata[]>;
 export function preview1AdapterCommandPath(): URL;
 export function preview1AdapterReactorPath(): URL;
 export { optimizeComponent as opt } from "./cmd/opt.js";
+import { tools } from "../obj/wasm-tools.js";
 export { transpileComponent as transpile, typesComponent as types } from "./cmd/transpile.js";
 //# sourceMappingURL=api.d.ts.map
