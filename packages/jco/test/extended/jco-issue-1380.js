6920b0779f3c49f58de3bc74200fdcfe5f13b29b
Victor Adossi <vadossi@cosmonic.com>
2026-06-02 01:30:59 +0900

    test(jco): add extended test suite for niche regression tests
    This commit adds an extended test suites that uses components in
    `jco/test/components` to ensure Jco functionality is working properly.
    
    While most components that are tested with Jco are written in either
    JS or Rust, all other components (e.g. C++, Python) will be stored in
    `jco/test/components` and built/used from there by relevant tests.

---
+++ packages/jco/test/extended/jco-issue-1380.js
@@ -0,0 +1,18 @@
+import { join } from "node:path";
+
+import { suite, test, assert } from "vitest";
+
+import { exec, jcoPath, fileExists } from "../helpers.js";
+import { EXTENDED_TEST_COMPONENTS_DIR } from "../common.js";
+
+suite("jco-issue-1380", () => {
+    test("composed component runs", async () => {
+        const combinedComponentPath = join(EXTENDED_TEST_COMPONENTS_DIR, "jco-issue-1380/composed.wasm");
+        assert(await fileExists(combinedComponentPath), "built composed component must be in place");
+
+        const { stdout, stderr } = await exec(jcoPath, "run", combinedComponentPath);
+
+        assert.strictEqual(stdout, "");
+        assert.strictEqual(stderr, "");
+    });
+});
--- test/components/python/jco-issue-1380/callee/component.py
+++ test/components/python/jco-issue-1380/callee/component.py
@@ -15,32 +15,21 @@ class Handle(iface.Handle):
     def __init__(self, path: str) -> None:
         self._path = path
         Handle._storage.setdefault(self._path, "")
-        print(f"[dbg] py: Handle.__init__ path={self._path}")
 
     def append(self, data: str) -> None:
-        print(f"[dbg] py: Handle.append begin path={self._path} data={data}")
         Handle._storage[self._path] = Handle._storage.get(self._path, "") + data
-        print(
-            f"[dbg] py: Handle.append end path={self._path} size={len(Handle._storage[self._path])}"
-        )
 
 
 class Iface(exports.Iface):
     def open_temp(self) -> Handle:
-        print("[dbg] py: open_temp begin")
         Handle._seq += 1
         h = Handle(f"mem://h-{Handle._seq}")
-        print(f"[dbg] py: open_temp end path={h._path}")
         return h
 
 
 class Run(exports.Run):
     def run(self) -> None:
-        print("[dbg] py: run begin")
         api = Handle()
-        print("[dbg] py: run open_temp")
         h = api.open_temp()
-        print("[dbg] py: run append")
         h.append("|py-run|")
-        print("[dbg] py: run end")
         return None
