bf355a7136b52db28b0ffdb43f84e47a3a60cc65
Victor Adossi <vadossi@cosmonic.com>
2026-06-08 21:08:18 +0900

    test(jco): add test setup & checks

--- packages/jco/test/p3/ported/component-model/wast.js
+++ packages/jco/test/p3/ported/component-model/wast.js
@@ -1,9 +1,10 @@
 import { join, relative } from "node:path";
 import { opendir } from "node:fs/promises";
+import { spawn } from "node:child_process";
 
-import { suite, test, assert } from "vitest";
+import { suite, test, assert, beforeAll } from "vitest";
 
-import { COMPONENT_MODEL_FIXTURES_WAST_DIR } from "../../../common.js";
+import { fileExists, COMPONENT_MODEL_FIXTURES_WAST_DIR } from "../../../common.js";
 
 // These tests are ported from the component-model repo
 //
@@ -11,15 +12,39 @@ import { COMPONENT_MODEL_FIXTURES_WAST_DIR } from "../../../common.js";
 //
 suite("component-model", async () => {
     const walker = await opendir(COMPONENT_MODEL_FIXTURES_WAST_DIR, { recursive: true });
+    const metadata = [];
     for await (const dirent of walker) {
         if (!dirent.isFile) {
             continue;
         }
-        const relPath = relative(COMPONENT_MODEL_FIXTURES_WAST_DIR, join(dirent.parentPath, dirent.name));
+        const wastPath = join(dirent.parentPath, dirent.name);
+        const wastRelPath = relative(COMPONENT_MODEL_FIXTURES_WAST_DIR, wastPath);
+        const wasmPath = join(COMPONENT_MODEL_FIXTURES_WAST_DIR, `${dirent.name}.wasm`);
+        const scriptPath = join(COMPONENT_MODEL_FIXTURES_WAST_DIR, `${dirent.name}.js`);
+        metadata.push({
+            wastRelPath,
+            wastPath,
+            wasmPath,
+            scriptPath,
+        });
+    }
 
-        test.concurrent(relPath, async () => {
-            // TODO: convert WAST test to WAT + executable JS
-            console.log(`path [${relPath}]`);
+    beforeAll(async () => {
+        for (const { wastPath } of metadata) {
+            const fixtureBuild = spawn("cargo", ["xtask", "build-wast-fixture", wastPath], {
+                detached: false,
+                stdio: "pipe",
+                shell: true,
+            });
+            await new Promise(resolve => fixtureBuild.on("exit", resolve));
+        }
+    });
+
+    for (const { wastRelPath, wasmPath, scriptPath } of metadata) {
+        test.concurrent(wastRelPath, async () => {
+            assert(await fileExists(scriptPath), `missing generated script @ [${scriptPath}]`);
+            assert(await fileExists(wasmPath), `missing generated wasm component @ [${wasmPath}]`);
+            // TODO: convert WAST test to WAST + executable JS
             assert.strictEqual(true, true);
         });
     }
