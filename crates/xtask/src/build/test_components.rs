4ba7edabceb114c4f1949a54b90e843fb6c8877c
Victor Adossi <vadossi@cosmonic.com>
2026-06-28 08:58:54 +0900

    chore(deps): remove test components from workspace
    This commit removes the test components from the workspace which
    should make compile times a little quicker and allow for use of
    different dependencies for test components more easily (e.g. newer
    versions of `wit-bindgen`)

--- crates/xtask/src/build/test_components.rs
+++ crates/xtask/src/build/test_components.rs
@@ -12,16 +12,19 @@ static WORKSPACE_DIR: LazyLock<PathBuf> =
 /// Building the `test-components/artifacts` project is what actually builds
 /// the code that is in the `test-programs` crate (i.e. via the build.rs script)
 pub(crate) fn run() -> Result<()> {
-    let _ = WORKSPACE_DIR;
     let sh = Shell::new().context("failed to build shell")?;
-    cmd!(sh, "cargo build -p jco-test-components")
+    sh.change_dir(WORKSPACE_DIR.join("./crates/test-components"));
+    cmd!(sh, "cargo build")
         .read()
         .context("failed to read output from cargo build")?;
-    cmd!(sh, "cargo clean -p jco-test-components-artifacts")
+
+    sh.change_dir(WORKSPACE_DIR.join("./crates/test-components/artifacts"));
+    cmd!(sh, "cargo clean")
         .read()
         .context("failed to clean output from cargo build")?;
-    cmd!(sh, "cargo build -p jco-test-components-artifacts")
+    cmd!(sh, "cargo build")
         .read()
         .context("failed to read output from cargo build")?;
+
     Ok(())
 }
