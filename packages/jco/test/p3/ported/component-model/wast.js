be233c9ccb68d29db2f51d70bc563b67e513edbc
Victor Adossi <vadossi@cosmonic.com>
2026-06-29 13:41:52 +0900

    test(jco): rename fixture wat directory to wast

--- packages/jco/test/p3/ported/component-model/wast.js
+++ packages/jco/test/p3/ported/component-model/wast.js
@@ -54,11 +54,7 @@ suite("component-model", async () => {
     });
 
     for (const { wastRelPath, wasmPath, scriptPath } of metadata) {
-        // const t = TESTS_TO_SKIP.has(wastRelPath) ? test.skip : test.concurrent;
-        //
-        // TODO: re-enable WAST tests once imported tests are passing/fixes have landed
-        // to enable them.
-        //
+        const t = TESTS_TO_SKIP.has(wastRelPath) ? test.skip : test.concurrent;
         test.skip(wastRelPath, async () => {
             assert(await fileExists(wasmPath), `missing generated wasm component @ [${wasmPath}]`);
             assert(await fileExists(scriptPath), `missing generated script @ [${scriptPath}]`);
