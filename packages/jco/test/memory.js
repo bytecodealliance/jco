1a82ff474b7f6a4535936b03f434d7830a168f58
Victor Adossi <vadossi@cosmonic.com>
2026-06-29 10:27:37 +0900

    test(jco): add regression test for subtask removal memory leak

--- packages/jco/test/memory.js
+++ packages/jco/test/memory.js
@@ -1,20 +1,23 @@
-import { fileURLToPath, URL } from "node:url";
-import { version } from "node:process";
-import { tmpdir } from "node:os";
-import { mkdir, writeFile, mkdtemp } from "node:fs/promises";
 import { dirname, join } from "node:path";
+import { fileURLToPath, URL } from "node:url";
+import { memoryUsage } from "node:process";
+import { mkdir, writeFile, mkdtemp } from "node:fs/promises";
+import { tmpdir } from "node:os";
+import { version } from "node:process";
 
 import { suite, test, assert } from "vitest";
+import { WASIShim } from "@bytecodealliance/preview2-shim/instantiation";
 
 import { transpile } from "../src/api.js";
 
-import { readComponentBytes, exec } from "./helpers.js";
+import { LOCAL_TEST_COMPONENTS_DIR } from "./common.js";
+import { readComponentBytes, exec, setupAsyncTest } from "./helpers.js";
 
 const ADDER_COMPONENT_PATH = fileURLToPath(new URL("./fixtures/components/adder.component.wasm", import.meta.url));
 
 const CALL_COUNT = 1_000_000;
 
-suite("Memory", () => {
+suite("memory", () => {
     // see: https://github.com/bytecodealliance/jco/issues/937
     test("does not leak during numerous transpile calls", async () => {
         let nodeMajorVersion = parseInt(version.replace("v", "").split(".")[0]);
@@ -67,5 +70,42 @@ process.stdout.write(JSON.stringify({ runs, memoryUsage: { before, after } }));
             afterUsageRSS < beforeUsageRSS + expectedUsageMB,
             `< 30MB used for extra memory (actual ${actualUsageMB}MB)`,
         );
+    });
+
+    // see: https://github.com/bytecodealliance/jco/issues/1675
+    test("host call loop", async () => {
+        let ticks = 1_000;
+        let memGrowthLimitMB = 1000 * 1000 * 10;
+
+        const name = "host-call-loop-sync";
+        const { instance, cleanup } = await setupAsyncTest({
+            component: {
+                path: join(LOCAL_TEST_COMPONENTS_DIR, `${name}.wasm`),
+                imports: {
+                    ...new WASIShim().getImportObject(),
+                    "jco:test-components/tick": {
+                        tick: () => {
+                            ticks -= 1;
+                        },
+                    },
+                },
+            },
+            // jco: {
+            //     transpile: {
+            //         extraArgs: {
+            //             minify: false,
+            //         },
+            //     },
+            // },
+        });
+
+        const before = memoryUsage();
+        instance["jco:test-components/local-run-n"].run(ticks);
+        assert.strictEqual(ticks, 0);
+        const after = memoryUsage();
+        const bytesUsed = after.rss - before.rss;
+        assert.isBelow(bytesUsed, memGrowthLimitMB, "no more than 1MB should be additionally used");
+
+        await cleanup();
     });
 });
