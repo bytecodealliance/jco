e177c911f5141bc94fccbe9492446dbd30e3ba03
Victor Adossi <vadossi@cosmonic.com>
2026-06-29 13:54:37 +0900

    test(jco): refactor to list of explicit tests rather than walking

--- packages/jco/test/p3/ported/component-model/wast.js
+++ packages/jco/test/p3/ported/component-model/wast.js
@@ -1,5 +1,4 @@
-import { join, relative, basename } from "node:path";
-import { opendir } from "node:fs/promises";
+import { join, basename } from "node:path";
 import { spawn } from "node:child_process";
 
 import { suite, test, assert, expect, beforeAll } from "vitest";
@@ -8,54 +7,60 @@ import { COMPONENT_MODEL_FIXTURES_WAST_DIR } from "../../../common.js";
 import { fileExists, setupAsyncTest } from "../../../helpers.js";
 
 // Relative paths to tests that should be skipped
-const TESTS_TO_SKIP = new Set([
-    // NOTE: this test must be skipped until we update upstream libs
-    "validation/implements.wast",
-]);
+const WAST_TESTS = [
+    { relPath: "async/wait-during-callback.wast", skip: true },
+    { relPath: "async/cancel-stream.wast", skip: true },
+    { relPath: "async/sync-streams.wast", skip: true },
+    { relPath: "async/deadlock.wast", skip: true },
+    { relPath: "async/trap-if-block-and-sync.wast", skip: true },
+    { relPath: "async/partial-stream-copies.wast", skip: true },
+    { relPath: "async/trap-on-reenter.wast", skip: true },
+    { relPath: "async/sync-barges-in.wast", skip: true },
+    { relPath: "async/same-component-stream-future.wast", skip: true },
+    { relPath: "async/futures-must-write.wast", skip: true },
+    { relPath: "async/dont-block-start.wast", skip: true },
+    { relPath: "async/cross-abi-calls.wast", skip: true },
+    { relPath: "async/empty-wait.wast", skip: true },
+    { relPath: "async/zero-length.wast", skip: true },
+    { relPath: "async/cancel-subtask.wast", skip: true },
+    { relPath: "async/closed-stream.wast", skip: true },
+    { relPath: "async/passing-resources.wast", skip: true },
+    { relPath: "async/drop-cross-task-borrow.wast", skip: true },
+    { relPath: "async/trap-if-done.wast", skip: true },
+    { relPath: "async/drop-waitable-set.wast", skip: true },
+    { relPath: "async/drop-stream.wast", skip: true },
+    { relPath: "async/async-calls-sync.wast", skip: true },
+    { relPath: "async/drop-subtask.wast", skip: true },
+    { relPath: "async/cancellable.wast", skip: true },
+];
 
 // These tests are ported from the component-model repo
 //
 // see: https://github.com/WebAssembly/component-model/tree/main/test
 //
-suite("component-model", async () => {
-    let metadata = [];
-    const walker = await opendir(COMPONENT_MODEL_FIXTURES_WAST_DIR, { recursive: true });
-
-    for await (const dirent of walker) {
-        if (!dirent.isFile() || !dirent.name.endsWith(".wast")) {
-            continue;
-        }
-
-        const wastPath = join(dirent.parentPath, dirent.name);
-        const wastRelPath = relative(COMPONENT_MODEL_FIXTURES_WAST_DIR, wastPath);
-        if (TESTS_TO_SKIP.has(wastRelPath)) {
-            continue;
-        }
-
-        const wasmPath = join(dirent.parentPath, `${dirent.name}.wasm`);
-        const scriptPath = join(dirent.parentPath, `${dirent.name}.js`);
-        metadata.push({
-            wastRelPath,
-            wastPath,
-            wasmPath,
-            scriptPath,
-        });
-    }
-
+suite("", async () => {
     beforeAll(async () => {
-        for (const { wastPath } of metadata) {
+        for (const { relPath, skip } of WAST_TESTS) {
+            if (skip) {
+                continue;
+            }
+            const wastPath = join(COMPONENT_MODEL_FIXTURES_WAST_DIR, relPath);
             const fixtureBuild = spawn("cargo", ["xtask", "build-wast-fixture", wastPath], {
                 detached: false,
                 stdio: "inherit",
                 shell: true,
             });
+            // TODO: handle xtask build failure
             await new Promise((resolve) => fixtureBuild.on("exit", resolve));
         }
     });
 
-    for (const { wastRelPath, wasmPath, scriptPath } of metadata) {
-        const t = TESTS_TO_SKIP.has(wastRelPath) ? test.skip : test.concurrent;
-        test.skip(wastRelPath, async () => {
+    for (const { relPath, skip } of WAST_TESTS) {
+        const wasmPath = join(COMPONENT_MODEL_FIXTURES_WAST_DIR, relPath.replace(/.wast$/, ".wast.wasm"));
+        const scriptPath = join(COMPONENT_MODEL_FIXTURES_WAST_DIR, relPath.replace(/.wast$/, ".js"));
+
+        const t = skip ? test.skip : test.concurrent;
+        t(relPath, async () => {
             assert(await fileExists(wasmPath), `missing generated wasm component @ [${wasmPath}]`);
             assert(await fileExists(scriptPath), `missing generated script @ [${scriptPath}]`);
 
@@ -64,16 +69,16 @@ suite("component-model", async () => {
                 const setup = await setupAsyncTest({
                     asyncMode: "jspi",
                     component: {
-                        name: basename(wastRelPath).replace(".wast.wasm", ""),
+                        name: basename(relPath).replace(".wast", ""),
                         path: wasmPath,
                     },
-                    jco: {
-                        transpile: {
-                            extraArgs: {
-                                minify: false,
-                            },
-                        },
-                    },
+                    // jco: {
+                    //     transpile: {
+                    //         extraArgs: {
+                    //             minify: false,
+                    //         },
+                    //     },
+                    // },
                 });
                 cleanup = setup.cleanup;
                 const instance = setup.instance;
