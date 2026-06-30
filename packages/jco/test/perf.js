a8620a60809b1e0bede969f81db01279b5cdc7c3
Victor Adossi <vadossi@cosmonic.com>
2026-06-30 20:29:52 +0900

    test(jco): enable tests for recent transpile fixes

--- packages/jco/test/perf.js
+++ packages/jco/test/perf.js
@@ -1,5 +1,5 @@
 import { join } from "node:path";
-import { hrtime } from "node:process";
+import { hrtime, env } from "node:process";
 
 import { suite, test, assert } from "vitest";
 
@@ -9,10 +9,11 @@ import { setupAsyncTest } from "./helpers.js";
 import { AsyncFunction, LOCAL_TEST_COMPONENTS_DIR } from "./common.js";
 import { composeCallerCallee } from "./p3/ported/wasmtime/component-async/common.js";
 
+const ASYNC_G2G_CALL_LIMIT_NS = env.CI ? 50_000_000 : 40_000_000;
+
 suite("performance", () => {
     // https://github.com/bytecodealliance/jco/issues/1711
-    // TODO: re-enable once transpile has been released
-    test.skip("guest->guest async call latency", async () => {
+    test.concurrent("guest->guest async call latency", async () => {
         // Build a combined component that will exercise the PrepareCall -> AsyncStartCall
         // path for guest->guest async calls
         const callerPath = join(LOCAL_TEST_COMPONENTS_DIR, "async-call-g2g-caller.wasm");
@@ -41,12 +42,15 @@ suite("performance", () => {
 
         assert.instanceOf(instance["jco:test-components/local-run-async"].run, AsyncFunction);
         const runs = 1_000;
-        const limitNs = 40000000;
         for (var current = 0; current < runs; current++) {
             const before = hrtime();
             await instance["jco:test-components/local-run-async"].run();
             const [seconds, ns] = hrtime(before);
-            assert.isBelow(seconds * 1e9 + ns, limitNs, `no run should take more than ${limitNs}ns`);
+            assert.isBelow(
+                seconds * 1e9 + ns,
+                ASYNC_G2G_CALL_LIMIT_NS,
+                `no run should take more than ${ASYNC_G2G_CALL_LIMIT_NS}ns`,
+            );
         }
         await cleanup();
     });
