2239af421d08d23cea852ec4542d8d1b28df3ae8
Victor Adossi <vadossi@cosmonic.com>
2026-07-29 19:24:09 +0900

    test(jco): add test for CM destructor metadata fix

--- packages/jco/test/componentize.js
+++ packages/jco/test/componentize.js
@@ -15,6 +15,38 @@ import { exec, getTmpDir, jcoPath } from "./helpers.js";
 // used widely, we can switch to regular dynamic imports, as componentize-js
 // versions are real dependencies now.
 suite("componentize", () => {
+    // ComponentizeJS currently omits the component-model destructor metadata for
+    // a JS-exported resource.
+    //
+    // TODO: flip this test once componentize-js is fixed
+    // see: https://github.com/bytecodealliance/jco/issues/989#issuecomment-4550236825
+    test.fails("componentized exported resource invokes its guest destructor", async () => {
+        const fixtureDir = join(COMPONENT_JS_FIXTURES_DIR, "resource-disposal");
+        const outputDir = await getTmpDir();
+        const componentPath = join(outputDir, "component.wasm");
+        const transpiledDir = join(outputDir, "transpiled");
+
+        await exec(
+            jcoPath,
+            "componentize",
+            join(fixtureDir, "source.js"),
+            "--disable",
+            "all",
+            "-w",
+            join(fixtureDir, "source.wit"),
+            "-o",
+            componentPath,
+        );
+        await exec(jcoPath, "transpile", componentPath, "-o", transpiledDir, "--name", "resource-disposal");
+        await writeFile(join(transpiledDir, "package.json"), JSON.stringify({ type: "module" }));
+
+        const { resources } = await import(`${pathToFileURL(transpiledDir)}/resource-disposal.js`);
+        const resource = new resources.Example(42);
+        assert.strictEqual(resource.getId(), 42);
+        resource[Symbol.dispose || Symbol.for("dispose")]();
+        assert.strictEqual(resources.disposeCount(), 1);
+    });
+
     test("rejects a custom StarlingMonkey engine with the QuickJS backend", async () => {
         const fixtureDir = join(COMPONENT_JS_FIXTURES_DIR, "typescript-direct");
         const outputDir = await getTmpDir();
