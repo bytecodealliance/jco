6102bc851967b1c41368816df6cfa1b5f9d70730
Victor Adossi <vadossi@cosmonic.com>
2026-08-20 20:50:46 +0900

    test(jco): unskip tests that should now pass

--- packages/jco/test/extended/jco-issue-1887.js
+++ packages/jco/test/extended/jco-issue-1887.js
@@ -8,8 +8,7 @@ import { exec, jcoPath, fileExists, getTmpDir } from "../helpers.js";
 import { EXTENDED_TEST_COMPONENTS_DIR } from "../common.js";
 
 suite("jco-issue-1887", () => {
-    // TODO: re-enable this test once jco-transpile has been released w/ fix
-    test.skip("WASI P3 reactor initializes and runs", async () => {
+    test("WASI P3 reactor initializes and runs", async () => {
         const componentPath = join(EXTENDED_TEST_COMPONENTS_DIR, "jco-issue-1887/component.wasm");
         assert(await fileExists(componentPath), "built issue component must be in place");
 
