332b4d8cd6a2965e0030dc5b423c93769102034d
Victor Adossi <vadossi@cosmonic.com>
2026-08-31 14:41:09 +0000

    test(transpile): enable drop-waitable-set WAST test

--- packages/jco-transpile/test/p3/ported/component-model/wast.ts
+++ packages/jco-transpile/test/p3/ported/component-model/wast.ts
@@ -37,7 +37,7 @@ const WAST_TESTS: readonly WastTest[] = [
     { relPath: 'async/passing-resources.wast', skip: true },
     { relPath: 'async/drop-cross-task-borrow.wast', skip: true },
     { relPath: 'async/trap-if-done.wast', skip: true },
-    { relPath: 'async/drop-waitable-set.wast', skip: true },
+    { relPath: 'async/drop-waitable-set.wast', skip: false },
     { relPath: 'async/drop-stream.wast', skip: true },
     { relPath: 'async/async-calls-sync.wast', skip: true },
     { relPath: 'async/drop-subtask.wast', skip: true },
