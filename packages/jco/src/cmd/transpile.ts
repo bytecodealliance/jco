5f70d872662393c3fcd0a0204aa0963f968c4b0d
Victor Adossi <vadossi@cosmonic.com>
2026-08-31 13:25:59 +0000

    feat(jco): deny Node DNS by default

--- packages/jco/src/cmd/transpile.ts
+++ packages/jco/src/cmd/transpile.ts
@@ -11,6 +11,7 @@ const DEFAULT_NODE_CAPABILITY_MAP = {
     "jco:node/child-process@0.1.0": "@bytecodealliance/jco-std/wasi/0.2.x/node/24.x.x/child-process/host",
     "jco:node/cluster@0.1.0": "@bytecodealliance/jco-std/wasi/0.2.x/node/24.x.x/cluster/host",
     "jco:node/console@0.1.0": "@bytecodealliance/jco-std/wasi/0.2.x/node/24.x.x/console/host",
+    "jco:node/dns@0.1.0": "@bytecodealliance/jco-std/wasi/0.2.x/node/24.x.x/dns/host",
 };
 
 /** Apply deny-by-default host mappings while preserving explicit application choices. */
