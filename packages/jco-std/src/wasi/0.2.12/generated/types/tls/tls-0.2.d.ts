873027f8d327cc68c91a853b99bf90e48d403738
Victor Adossi <vadossi@cosmonic.com>
2026-09-07 14:11:56 +0000

    build(std): regenerate TLS bindings against WASI IO 0.2.12

--- packages/jco-std/src/wasi/0.2.12/generated/types/tls/tls-0.2.d.ts
+++ packages/jco-std/src/wasi/0.2.12/generated/types/tls/tls-0.2.d.ts
@@ -3,8 +3,8 @@
 /// <reference path="./interfaces/wasi-io-streams.d.ts" />
 /// <reference path="./interfaces/wasi-tls-types.d.ts" />
 declare module 'wasi:tls/imports@0.2.0-draft' {
-  export type * as WasiIoError026 from 'wasi:io/error@0.2.6'; // import wasi:io/error@0.2.6
-  export type * as WasiIoPoll026 from 'wasi:io/poll@0.2.6'; // import wasi:io/poll@0.2.6
-  export type * as WasiIoStreams026 from 'wasi:io/streams@0.2.6'; // import wasi:io/streams@0.2.6
+  export type * as WasiIoError0212 from 'wasi:io/error@0.2.12'; // import wasi:io/error@0.2.12
+  export type * as WasiIoPoll0212 from 'wasi:io/poll@0.2.12'; // import wasi:io/poll@0.2.12
+  export type * as WasiIoStreams0212 from 'wasi:io/streams@0.2.12'; // import wasi:io/streams@0.2.12
   export type * as WasiTlsTypes020Draft from 'wasi:tls/types@0.2.0-draft'; // import wasi:tls/types@0.2.0-draft
 }
