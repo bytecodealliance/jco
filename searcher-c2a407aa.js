67a7f21121a54d0af203efa5f746e934b49d4bff
vados-cosmonic <vados-cosmonic@users.noreply.github.com>
2026-09-08 15:35:54 +0000

    deploy: ba19959994ee61da7f4efc1bfce31c20e48b05a2

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-2c51b535.js',
+                path_to_root + 'searchindex-941692ed.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
