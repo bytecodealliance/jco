1020b706cabbd3a822b2786aa7cf98ec75ee80c5
Sy Brand <sy.brand@fastly.com>
2026-09-09 10:28:39 +0100

    docs(tests): simplify readme

--- packages/jco/test/extended/posix-pthread-mutex-lock-1-1.js
+++ packages/jco/test/extended/posix-pthread-mutex-lock-1-1.js
@@ -10,10 +10,7 @@ import { EXTENDED_TEST_COMPONENTS_DIR } from "../common.js";
 suite("posix-pthread-mutex-lock", () => {
     // TODO(unskip): enable once jco implements the Component Model threading intrinsics.
     test.skip("1-1", async () => {
-        const componentPath = join(
-            EXTENDED_TEST_COMPONENTS_DIR,
-            "posix/pthread-mutex-lock/1-1/component.wasm",
-        );
+        const componentPath = join(EXTENDED_TEST_COMPONENTS_DIR, "posix/pthread-mutex-lock/1-1/component.wasm");
         assert(await fileExists(componentPath), "built posix-pthread-mutex-lock-1-1 component must be in place");
 
         const outputDir = await getTmpDir();
