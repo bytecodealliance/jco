c588db9d080f81c28bdaeb6f0ca813dfd64ac0d4
Victor Adossi <vadossi@cosmonic.com>
2026-09-10 10:12:31 +0000

    refactor(std): inline process source attribution

--- packages/jco-std/src/wasi/0.2.x/node/24.x.x/process/validation.ts
+++ packages/jco-std/src/wasi/0.2.x/node/24.x.x/process/validation.ts
@@ -1,6 +1,29 @@
+/*!
+ * Portions of this directory are adapted from Node.js and DefinitelyTyped.
+ * Copyright Node.js contributors. All rights reserved.
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 /** Validation and WIT error reconstruction for the process facade.
  * previous() follows Node v24.20.0 internal/process/per_thread.js (MIT),
- * commit 71b8b174857e25106d39b61a9e6f30d927da8b01. See license.ts.
+ * commit 71b8b174857e25106d39b61a9e6f30d927da8b01.
  */
 import { decodeErrno } from "../internal/host-error.js";
 import {
